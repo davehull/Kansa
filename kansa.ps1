f488dc62e55b004d5c29322b9b87ac90abe6566a
Dave Hull <dpgit@trustedsignal.com>
2014-03-09 23:32:48 -0700

    Get-Modules function.

--- kansa.ps1
+++ kansa.ps1
@@ -16,10 +16,49 @@ You can search for RSAT at:
 
 http://www.microsoft.com/en-us/download/default.aspx
 .PARAMETER ModulePath
-Specifies the path to the collector modules.
+An optional parameter that specifies the path to the collector modules.
 .PARAMETER OutputPath
-Specifies the main output path. Each host's output will be written
-to subdirectories beneath the main output path.
+An optional parameter that specifies the main output path. Each host's 
+output will be written to subdirectories beneath the main output path.
 .EXAMPLE
 Kansa.ps1 -ModulePath .\Kansas -OutputPath .\AtlantaDataCenter\
 #>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$False,Position=0)]
+        [String]$ModulePath="Modules\",
+    [Parameter(Mandatory=$False,Position=1)]
+        [String]$OutputPath="OutputPath\"
+)
+
+Write-Debug "`$ModulePath is ${ModulePath}."
+Write-Debug "`$OutputPath is ${OutputPath}."
+
+if (-not (Test-Path($ModulePath))) {
+    Write-Error -Category InvalidArgument -Message "User supplied ModulePath, $ModulePath, was not found."
+    Exit
+}
+
+if (-not (Test-Path($OutputPath))) {
+    Write-Error -Category InvalidArgument -Message "User supplied OutputPath, $OutputPath, was not found."
+    Exit
+}
+
+$ErrorLog = $OutputPath + "\Error.Log"
+
+function Get-Modules {
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [String]$ModulePath
+)
+    Write-Verbose "Entering $($MyInvocation.MyCommand)"
+    Try {
+        ls -r $ModulePath
+    } Catch [Exception] {
+        $_.Exception.GetType().FullName | Add-Content -Encoding Ascii $ErrorLog
+        $_.Exception.Message | Add-Content -Encoding Ascii $ErrorLog
+    }
+}
+
+Get-Modules $ModulePath

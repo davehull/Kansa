279d0f0939501101d0ab4a1d72e35a8bf63b44f2
Dave Hull <dpgit@trustedsignal.com>
2014-03-26 13:25:43 -0700

    Adding modules.

---
+++ Modules/Get-PrefetchListing.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+Get listing of prefetch files
+#>
+
+$pfconf = (Get-ItemProperty "hklm:\system\currentcontrolset\control\session manager\memory management\prefetchparameters").EnablePrefetcher 
+Switch -Regex ($pfconf) {
+    "[1-3]" {
+        ls $env:windir\Prefetch\*.pf
+    }
+    default {
+        Write-Output "Prefetch not enabled on ${env:COMPUTERNAME}."
+    }
+}

2a9589a9fe27c19bd7ffacce349be85f630a0eb4
Dave Hull <dpgit@trustedsignal.com>
2014-04-12 16:15:38 -0700

    Added Get-Netstat.ps1 module with conversion to tsv.

---
+++ Modules/Get-Netstat.ps1
@@ -0,0 +1,59 @@
+﻿$Ext = ".tsv"
+<#
+.SYNOPSIS
+Get-Netstat.ps1 acquires netstat -naob output and reformats on the 
+target as tsv output.
+#>
+
+function Get-AddrPort {
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [String]$AddrPort
+)
+    Write-Verbose "Entering $($MyInvocation.MyCommand)"
+    Write-Verbose "Processing $AddrPort"
+    if ($AddrPort -match '[0-9a-f]*:[0-9a-f]*:[0-9a-f%]*\]:[0-9]+') {
+        $Addr, $Port = $AddrPort -split "]:"
+        $Addr += "]"
+    } else {
+        $Addr, $Port = $AddrPort -split ":"
+    }
+    $Addr, $Port
+    Write-Verbose "Exiting $($MyInvocation.MyCommand)"
+}
+
+("Protocol","LocalAddress","LocalPort","ForeignAddress","ForeignPort","State","PId","Component","Process") -join "`t"
+foreach($line in $(& $env:windir\system32\netstat.exe -naob)) {
+    if ($line.length -gt 1 -and $line -notmatch "Active |Proto ") {
+        $line = $line.trim()
+        if ($line.StartsWith("TCP")) {
+            $Protocol, $LocalAddress, $ForeignAddress, $State, $ConPId = ($line -split '\s{2,}')
+            $Component = $Process = $False
+        } elseif ($line.StartsWith("UDP")) { 
+            $State = "STATELESS"
+            $Protocol, $LocalAddress, $ForeignAddress, $ConPid = ($line -split '\s{2,}')
+            $Component = $Process = $False
+        } elseif ($line -match "^\[[-_a-zA-Z0-9.]+\.(exe|com|ps1)\]$") {
+            $Process = $line
+            if ($Component -eq $False) {
+                # No Component given
+                $Component = $Process
+            }
+        } elseif ($line -match "Can not obtain ownership information") {
+            $Process = $Component = $line
+        } else {
+            # We have the $Component
+            $Component = $line
+        }
+        if ($State -match "TIME_WAIT") {
+            $Component = "Not provided"
+            $Process = "Not provided"
+        }
+        if ($Component -and $Process) {
+            $LocalAddress, $LocalPort = Get-AddrPort($LocalAddress)
+            $ForeignAddress, $ForeignPort = Get-AddrPort($ForeignAddress)
+            ($Protocol, $LocalAddress, $LocalPort, $ForeignAddress, $ForeignPort, $State, $ConPid, $Component, $Process) -join "`t"
+        }
+    }
+}
+

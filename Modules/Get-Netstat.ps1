fd07cf26c3bbf6e85c45f41cbc49f145a5726014
Dave Hull <dpgit@trustedsignal.com>
2014-04-14 15:21:33 -0700

    Updated Get-Netstat.ps1 to support both admin and non-admin priv levels, but each returns different data.

--- Modules/Get-Netstat.ps1
+++ Modules/Get-Netstat.ps1
@@ -25,38 +25,59 @@ Param(
     Write-Verbose "Exiting $($MyInvocation.MyCommand)"
 }
 
-("Protocol","LocalAddress","LocalPort","ForeignAddress","ForeignPort","State","PId","Component","Process") -join "`t"
-foreach($line in $(& $env:windir\system32\netstat.exe -naob)) {
-    if ($line.length -gt 1 -and $line -notmatch "Active |Proto ") {
-        $line = $line.trim()
-        if ($line.StartsWith("TCP")) {
-            $Protocol, $LocalAddress, $ForeignAddress, $State, $ConPId = ($line -split '\s{2,}')
-            $Component = $Process = $False
-        } elseif ($line.StartsWith("UDP")) { 
-            $State = "STATELESS"
-            $Protocol, $LocalAddress, $ForeignAddress, $ConPid = ($line -split '\s{2,}')
-            $Component = $Process = $False
-        } elseif ($line -match "^\[[-_a-zA-Z0-9.]+\.(exe|com|ps1)\]$") {
-            $Process = $line
-            if ($Component -eq $False) {
-                # No Component given
-                $Component = $Process
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+# If run as admin, collect Component and Process names in addition to other data.
+    $netstatScriptBlock = { & $env:windir\system32\netstat.exe -naob }
+    ("Protocol","LocalAddress","LocalPort","ForeignAddress","ForeignPort","State","PId","Component","Process") -join "`t"
+    foreach($line in $(& $netstatScriptBlock)) {
+        if ($line.length -gt 1 -and $line -notmatch "Active |Proto ") {
+            $line = $line.trim()
+            if ($line.StartsWith("TCP")) {
+                $Protocol, $LocalAddress, $ForeignAddress, $State, $ConPId = ($line -split '\s{2,}')
+                $Component = $Process = $False
+            } elseif ($line.StartsWith("UDP")) { 
+                $State = "STATELESS"
+                $Protocol, $LocalAddress, $ForeignAddress, $ConPid = ($line -split '\s{2,}')
+                $Component = $Process = $False
+            } elseif ($line -match "^\[[-_a-zA-Z0-9.]+\.(exe|com|ps1)\]$") {
+                $Process = $line
+                if ($Component -eq $False) {
+                    # No Component given
+                    $Component = $Process
+                }
+            } elseif ($line -match "Can not obtain ownership information") {
+                $Process = $Component = $line
+            } else {
+                # We have the $Component
+                $Component = $line
             }
-        } elseif ($line -match "Can not obtain ownership information") {
-            $Process = $Component = $line
-        } else {
-            # We have the $Component
-            $Component = $line
+            if ($State -match "TIME_WAIT") {
+                $Component = "Not provided"
+                $Process = "Not provided"
+            }
+            if ($Component -and $Process) {
+                $LocalAddress, $LocalPort = Get-AddrPort($LocalAddress)
+                $ForeignAddress, $ForeignPort = Get-AddrPort($ForeignAddress)
+                ($Protocol, $LocalAddress, $LocalPort, $ForeignAddress, $ForeignPort, $State, $ConPid, $Component, $Process) -join "`t"
+            }
         }
-        if ($State -match "TIME_WAIT") {
-            $Component = "Not provided"
-            $Process = "Not provided"
-        }
-        if ($Component -and $Process) {
+    }
+} else {
+# If run as non-admin, we can't grab Component and Process name.
+    $netstatScriptBlock = { & $env:windir\system32\netstat.exe -nao }
+    ("Protocol","LocalAddress","LocalPort","ForeignAddress","ForeignPort","State","PId") -join "`t"
+    foreach($line in $(& $netstatScriptBlock)) {
+        if ($line.length -gt 1 -and $line -notmatch "Active |Proto ") {
+            $line = $line.trim()
+            if ($line.StartsWith("TCP")) {
+                $Protocol, $LocalAddress, $ForeignAddress, $State, $ConPId = ($line -split '\s{2,}')
+            } elseif ($line.StartsWith("UDP")) {
+                $State = "STATELESS"
+                $Protocol, $LocalAddress, $ForeignAddress, $ConPId = ($line -split '\s{2,}')
+            }
             $LocalAddress, $LocalPort = Get-AddrPort($LocalAddress)
             $ForeignAddress, $ForeignPort = Get-AddrPort($ForeignAddress)
-            ($Protocol, $LocalAddress, $LocalPort, $ForeignAddress, $ForeignPort, $State, $ConPid, $Component, $Process) -join "`t"
+            ($Protocol, $LocalAddress, $LocalPort, $ForeignAddress, $ForeignPort, $State, $ConPId) -join "`t"
         }
     }
 }
-

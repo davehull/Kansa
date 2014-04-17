49abb32b77083bab1e41169e1e1902cd3befbea8
Dave Hull <dpgit@trustedsignal.com>
2014-04-17 11:28:40 -0700

    Get-Autorunsc and Get-Handle added. Don't like expecting binaries to be in systemroot, but will have to address that later.

---
+++ Modules/Get-Autorunsc.ps1
@@ -0,0 +1,20 @@
+﻿# OUTPUT txt
+# BINDEP Autorunsc.exe
+<#
+.SYNOPSIS
+Get-Autorunsc.ps1 depends on Sysinternals Autorunsc.exe, Kansa.ps1 should recognize this
+based on line two of this script and will attempt to copy it from $ModulePath\bin\ to the 
+remote system's ADMIN$ share. If the ADMIN$ share does not exist, the copy will fail and
+the module will be skipped.
+
+Users will have to download Autorunsc.exe or any other binaries separately, they are not
+included with Kansa.
+
+!!THIS SCRIPT ASSUMES AUTORUNSC.EXE WILL BE IN $ENV:SYSTEMROOT!!
+#>
+
+if (Test-Path "$env:SystemRoot\Autorunsc.exe") {
+    & $env:SystemRoot\Autorunsc.exe /accepteula -a -v -c -f '*'
+} else {
+    Write-Error "Autorunsc.exe not found in $env:SystemRoot."
+}
---
+++ Modules/Get-Handle.ps1
@@ -0,0 +1,51 @@
+﻿# OUTPUT tsv
+<# 
+Acquires handle data using sysinternals handle.exe
+
+!!THIS SCRIPT ASSUMES HANDLE.EXE WILL BE IN $ENV:SYSTEMROOT!!
+#>
+
+
+if (Test-Path "$env:SystemRoot\handle.exe") {
+    $data = (& $env:SystemRoot\handle.exe /accepteula -a)
+    ("Process","PId","Owner","Type","Perms","Name") -join $Delimiter
+    foreach($line in $data) {
+        $line = $line.Trim()
+        if ($line -match " pid: ") {
+            $HandleId = $Type = $Perms = $Name = $null
+            $pattern = "(?<ProcessName>^[-a-zA-Z0-9_.]+) pid: (?<PId>\d+) (?<Owner>.+$)"
+            if ($line -match $pattern) {
+                $ProcessName,$ProcId,$Owner = ($matches['ProcessName'],$matches['PId'],$matches['Owner'])
+            }
+        } else {
+            $pattern = "(?<HandleId>^[a-f0-9]+): (?<Type>\w+)"
+            if ($line -match $pattern) {
+                $HandleId,$Type = ($matches['HandleId'],$matches['Type'])
+                $Perms = $Name = $null
+                switch ($Type) {
+                    "File" {
+                        $pattern = "(?<HandleId>^[a-f0-9]+):\s+(?<Type>\w+)\s+(?<Perms>\([-RWD]+\))\s+(?<Name>.*)"
+                        if ($line -match $pattern) {
+                            $Perms,$Name = ($matches['Perms'],$matches['Name'])
+                        }
+                    }
+                    default {
+                        $pattern = "(?<HandleId>^[a-f0-9]+):\s+(?<Type>\w+)\s+(?<Name>.*)"
+                        if ($line -match $pattern) {
+                            $Name = ($matches['Name'])
+                        }
+                    }
+                }
+                if ($Name -ne $null) {
+                    $o = "" | Select-Object ProcessName, ProcId, Owner, Type, Perms, Name
+                    $o.ProcessName, $o.ProcId, $o.Owner, $o.Type, $o.Perms, $o.name = `
+                        $ProcessName,$ProcId,$Owner,$Type,$Perms,$Name
+                    $o
+                }
+            }
+        }
+    }
+
+} else {
+    Write-Error "Handle.exe not found in $env:SystemRoot."
+}

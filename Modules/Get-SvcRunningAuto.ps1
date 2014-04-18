36dd39fac79f12cb75752ae245e04353455d24d7
Dave Hull <dpgit@trustedsignal.com>
2014-04-18 10:32:43 -0700

    Two new collectors for services, some overlap with Autoruns data.

---
+++ Modules/Get-SvcRunningAuto.ps1
@@ -0,0 +1,7 @@
+﻿# OUTPUT xml
+<#
+Get-RunningServices.ps1
+Acquires details about running and auto-start services
+#>
+
+Get-WmiObject win32_service | ? { $_.State -match "Running" -or $_.StartMode -match "Auto" }
---
+++ Modules/Get-SvcsAll.ps1
@@ -0,0 +1,7 @@
+﻿# OUTPUT xml
+<#
+Get-AllServices.ps1
+Acquires details about running and auto-start services
+#>
+
+Get-WmiObject win32_service

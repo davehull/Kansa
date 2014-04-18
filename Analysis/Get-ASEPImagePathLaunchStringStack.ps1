635464b2712f7a98a03667c1889705d723cc9296
Dave Hull <dpgit@trustedsignal.com>
2014-04-18 16:00:33 -0700

    Modifying Get-DNSCache.ps1 to output objects. Adding an analysis stacker for ASEPs.

---
+++ Analysis/Get-ASEPImagePathLaunchStringStack.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Get-ASEPImagePathLaunchStringStack.ps1
+Requires logparser.exe in path
+Pulls frequency of autoruns based on [Image Path] and [Launch String] tuple
+ToDo: 
+Add check for logparser.exe in path
+Add check for *svctrigs.tsv in current path
+#>
+
+& logparser -i:csv -dtlines:0 -rtp:50 "select Count([Image Path], [Launch String]) as ct, [Image Path], [Launch String] from *autorunsc.txt where Publisher not like '(Verified)%' group by [Image Path], [Launch String] order by ct asc"
--- Modules/Get-DNSCache.ps1
+++ Modules/Get-DNSCache.ps1
@@ -1,8 +1,12 @@
-﻿# OUTPUT txt
+﻿# OUTPUT tsv
 <#
 .SYNOPSIS
 Get-DNSCache.ps1 acquires DNS cache entries from the target host.
 #>
 
 & ipconfig /displaydns | Select-String 'Record Name' | ForEach-Object { $_.ToString().Split(' ')[-1] } | `
-  Select-Object -Unique | sort 
+  Select-Object -Unique | sort | % {
+    $o = "" | Select-Object FQDN
+    $o.FQDN = $_
+    $o
+  }

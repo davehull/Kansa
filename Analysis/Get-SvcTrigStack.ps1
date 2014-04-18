38ec41d5c5c4f269f33711c8af83b2d212ecbc15
Dave Hull <dpgit@trustedsignal.com>
2014-04-18 08:49:48 -0700

    Another quick and dirty analysis script, requires logparser in path. Needs attention.

---
+++ Analysis/Get-SvcTrigStack.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Get-SvcTrigStack.ps1
+Requires logparser.exe in path
+Pulls stack rank of Service Triggers from acquired Service Trigger data
+ToDo: 
+Add check for logparser.exe in path
+Add check for *svctrigs.tsv in current path
+#>
+
+& logparser -i:tsv -fixedsep:on -dtlines:0 -rtp:50 "select Count(Condition, Value) as ct, ServiceName, Action, Condition, Value from *svctrigs.tsv group by ServiceName, Action, Condition, Value order by ct asc"

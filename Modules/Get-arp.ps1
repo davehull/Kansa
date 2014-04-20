becf7bd8b87ef318ecf0bc9deeb3d4d424dfef3b
Dave Hull <dpgit@trustedsignal.com>
2014-04-19 22:28:16 -0700

    Get-Arp.ps1 modified to return objects.

--- Modules/Get-arp.ps1
+++ Modules/Get-arp.ps1
@@ -1,5 +1,24 @@
-﻿# OUTPUT txt
+﻿# OUTPUT tsv
 <#
 arpa.ps1 acquires arp table
 #>
-& $env:windir\system32\arp.exe -a
+$IpPattern = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+foreach ($line in (& $env:windir\system32\arp.exe -a)) {
+    $line = $line.Trim()
+    if ($line.Length -gt 0) {
+        if ($line -match 'Interface:\s(?<Interface>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s.*') {
+            $Interface = $matches['Interface']
+        } elseif ($line -match '(?<IpAddr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?<Mac>[0-9A-Fa-f]{2}\-[0-9A-Fa-f]{2}\-[0-9A-Fa-f]{2}\-[0-9A-Fa-f]{2}\-[0-9A-Fa-f]{2}\-[0-9A-Fa-f]{2})*\s+(?<Type>dynamic|static)') {
+            $IpAddr = $matches['IpAddr']
+            if ($matches['Mac']) {
+                $Mac = $matches['Mac']
+            } else {
+                $Mac = ""
+            }
+            $Type   = $matches['Type']
+            $o = "" | Select-Object Interface, IpAddr, Mac, Type
+            $o.Interface, $o.IpAddr, $o.Mac, $o.Type = $Interface, $IpAddr, $Mac, $Type
+            $o
+        }
+    }
+}

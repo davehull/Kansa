1b5a7678ef25a0fba1935b35bf1170727b070e86
Dave Hull <dpgit@trustedsignal.com>
2014-04-25 20:55:46 -0700

    Removing the comment from Get-Prox.ps1

--- Modules/Get-Prox.ps1
+++ Modules/Get-Prox.ps1
@@ -8,11 +8,5 @@ to a bug mentioned here:
 http://connect.microsoft.com/PowerShell/feedback/details/767928/it-isnt-possible-to-override-the-default-global-serialization-depth-for-all-objects-in-remote-sessions-when-using-invoke-command-or-start-job
 #>
 
-<#
-Get-Process | Export-Clixml KansaProx.xml
-Get-Content -ReadCount 0 -Raw KansaProx.xml
-Remove-Item KansaProx.xml
-#>
-
 Update-TypeData -TypeName System.Diagnostics.Process -SerializationDepth 3 -Force
 Get-Process

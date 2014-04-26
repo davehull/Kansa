3fd5fa6c7965f1ab15e7ca6359e487e2aa3b15cb
Dave Hull <dpgit@trustedsignal.com>
2014-04-25 17:28:51 -0700

    And after being told the root cause of the issue with loss of fidelity in the Get-Process command, this is the right thing to do.

--- Modules/Get-Prox.ps1
+++ Modules/Get-Prox.ps1
@@ -1,21 +1,18 @@
-﻿# OUTPUT txt
+﻿# OUTPUT xml
 <#
 .SYNOPSIS
 Get-Prox.ps1 acquires Get-Process data
-I had to adjust the output of this module
-to txt, though in reality it is still xml.
-When Get-Process is run remotely, the thread
-property isn't preserved if the data is sent
-directly back to the host where the command 
-was invoked, but if the data is written to disk
-first on the remote host, the full fidelity of
-the thread property is preserved. A call to 
-Get-Content is then used to read it from disk
-causing it to be sent back to the calling host.
-Odd Powershell issue that I'd love to hear an
-explanation about.
+The Update-TypeData command is required in order
+to prevent deserialization of some of the data due
+to a bug mentioned here:
+http://connect.microsoft.com/PowerShell/feedback/details/767928/it-isnt-possible-to-override-the-default-global-serialization-depth-for-all-objects-in-remote-sessions-when-using-invoke-command-or-start-job
 #>
 
-Get-Process | Export-Clixml KansaProx.xml -Depth 10
+<#
+Get-Process | Export-Clixml KansaProx.xml
 Get-Content -ReadCount 0 -Raw KansaProx.xml
 Remove-Item KansaProx.xml
+#>
+
+Update-TypeData -TypeName System.Diagnostics.Process -SerializationDepth 3 -Force
+Get-Process

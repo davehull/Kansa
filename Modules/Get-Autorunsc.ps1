2ab748d4320acdafd6877048fa6aa91287a98ea2
Dave Hull <dpgit@trustedsignal.com>
2014-04-28 10:40:03 -0700

    Grr Autorunsc.exe output is a small mess. This improves on it somewhat and returns objects.

--- Modules/Get-Autorunsc.ps1
+++ Modules/Get-Autorunsc.ps1
@@ -1,4 +1,4 @@
-﻿# OUTPUT txt
+﻿# OUTPUT tsv
 # BINDEP Autorunsc.exe
 <#
 .SYNOPSIS
@@ -15,10 +15,21 @@ included with Kansa.
 If you have autorunsc.exe in your Modules\bin\ path and run the script with the -Pushbin
 flag, Kansa will attempt to copy the binary to the ADMIN$. Binaries are not removed, so
 subsequent runs won't require -Pushbin.
+
+Autorunsc output is a mess. Some of it is quoted csv, some is just csv. This script
+parses it pretty well, but there are still some things that are incorrectly parsed. As I
+have time, I'll see about resolving the troublesome entries, one in particular is for
+items that don't have time stamps.
 #>
 
 if (Test-Path "$env:SystemRoot\Autorunsc.exe") {
-    & $env:SystemRoot\Autorunsc.exe /accepteula -a -v -c -f '*' 2> $null
+    $data = & $env:SystemRoot\Autorunsc.exe /accepteula -a -v -c -f '*' 2> $null
+    $data | Select-Object -Skip 1 | % {
+        $row = $_ -replace "([A-Za-z0-9`"]),([A-Za-z0-9`"])", "`${1}`t`${2}" -replace ",,", "`t`t" -replace "(\s),$", "`${1}`t" -replace "`""
+        $o = "" | Select-Object Time, EntryLocation, Entry, Enabled, Category, Description, Publisher, ImagePath, Version, LaunchString, MD5, SHA1, PESHA1, PESHA256, SHA256
+        $o.Time, $o.EntryLocation, $o.Entry, $o.Enabled, $o.Category, $o.Description, $o.Publisher, $o.ImagePath, $o.Version, $o.LaunchString, $o.MD5, $o.SHA1, $o.PESHA1, $o.PESHA256, $o.SHA256 = ($row -split "`t")
+        $o
+    }
 } else {
     Write-Error "Autorunsc.exe not found in $env:SystemRoot."
 }

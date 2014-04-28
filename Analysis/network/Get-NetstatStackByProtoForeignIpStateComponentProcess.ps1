0c86a5a59038c4294e2a48a3d70c4873c8a933c4
Dave Hull <dpgit@trustedsignal.com>
2014-04-27 20:43:39 -0700

    Adding some simple netstat stacking scripts.

---
+++ Analysis/network/Get-NetstatStackByProtoForeignIpStateComponentProcess.ps1
@@ -0,0 +1,65 @@
+﻿<#
+Get-NetstatStackByProtoForeignIpStateComponentProcess.ps1
+Requires logparser.exe in path
+
+Pulls stack rank of connections from netstat data based on 
+Protocol, ForeignAddress, State, Component and Process. 
+Assumes local network space is using 10/8 CIDR and looks for
+foreign addresses outside that range. See query for additional
+filtered IP ranges and processes.
+
+
+You may want to customize this a bit for
+your environment. If you're running web 
+servers, for example, stacking them may be
+meaningless if they are meant to accept 
+connections from any host in the world, or
+maybe you just remove the web server process
+from the query...
+
+!! YOU WILL LIKELY WANT TO ADJUST THIS QUERY !!
+
+This script exepcts files matching the pattern 
+*netstat.tsv to be in the current working
+directory
+#>
+
+if (Get-Command logparser.exe) {
+
+    $lpquery = @"
+    SELECT
+        COUNT(Protocol,
+        ForeignAddress,
+        State,
+        Component,
+        Process) as ct,
+        Protocol,
+        ForeignAddress,
+        State,
+        Component,
+        Process
+    FROM
+        *netstat.tsv
+    WHERE
+        ConPid not in ('0'; '4') and
+        ForeignAddress not like '10.%' and
+        ForeignAddress not like '169.254%' and
+        ForeignAddress not in ('*'; '0.0.0.0'; 
+            '127.0.0.1'; '[::]'; '[::1]')
+    GROUP BY
+        Protocol,
+        ForeignAddress,
+        State,
+        Component,
+        Process
+    ORDER BY
+        Process,
+        ct desc
+"@
+
+    & logparser -i:tsv -fixedsep:on -dtlines:0 -rtp:50 $lpquery
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

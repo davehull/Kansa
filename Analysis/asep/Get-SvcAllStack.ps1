2fa7750651d4c76965939abe5729088abfd6f861
Dave Hull <dpgit@trustedsignal.com>
2014-05-02 16:59:30 -0700

    Adding a couple stackers for services.

---
+++ Analysis/asep/Get-SvcAllStack.ps1
@@ -0,0 +1,28 @@
+﻿<#
+Get-SvcAllStack.ps1
+A basic stack for services aggregating on Caption and Pathname.
+Out put is fairly ugly, it is sorted by Name, rather than count.
+Sorting this way means that items with the same Caption, but 
+different Pathnames will be reported next to each other. Here's
+an example:
+
+2 HP Version Control Age... {@{Caption=HP Version Control Agent; PathName="C:\hp\hpsmh\data\cgi-bin\vcagent\vcagent.exe"}, @{Caption=HP Version Control Agent; PathName="C:\hp\hpsmh\data\cgi-bin\vcagent\vcagent.exe"}}
+1 HP Version Control Age... {@{Caption=HP Version Control Agent; PathName=C:\hp\hpsmh\data\cgi-bin\vcagent\vcagent.exe}}
+
+Here we have scan resunts from three systems. All three have a
+service named "HP Version Control Agent,", but one of them has
+a pathname without double-quotes. A superficial difference.
+
+Get-Autorunsc.ps1 provides much of the same information, but
+Get-SvcAll.ps1 shows Process Ids for running processes and tells
+you which account the item is running under.
+
+#>
+
+$data = $null
+
+foreach ($file in (ls *svcall.xml)) {
+    $data += Import-Clixml $file
+}
+
+$data | Select-Object Caption, Pathname | Sort-Object Caption, Pathname | Group-Object Caption, Pathname | Sort-Object Name
---
+++ Analysis/asep/Get-SvcStartNameStack.ps1
@@ -0,0 +1,23 @@
+﻿<#
+Get-SvcStartNameStack.ps1
+A basic stack for services aggregating on Caption and StartName.
+StartName tells you what account the service runs under.
+
+Out put is fairly ugly, it is sorted by Name, rather than count.
+Sorting this way means that items with the same Caption, but 
+different StartNames will be reported next to each other. Seeing
+two services reporting to be the same service, but running under
+different accounts would be worthy of closer investigation, confirm
+the Pathnames are the same for both of them.
+
+Get-Autorunsc.ps1 output provides much of this same information,
+but it doesn't tell you the account a service will run under.
+#>
+
+$data = $null
+
+foreach ($file in (ls *svcall.xml)) {
+    $data += Import-Clixml $file
+}
+
+$data | Select-Object Caption, StartName | Sort-Object Caption, StartName | Group-Object Caption, StartName | Sort-Object Name

9171b41efaf50b8a4789bd773b143163a5be28c4
Dave Hull <dpgit@trustedsignal.com>
2014-05-03 14:49:26 -0700

    Updating Get-ProcDump.ps1 with more concise instructions.

--- Modules/Get-ProcDump.ps1
+++ Modules/Get-ProcDump.ps1
@@ -3,13 +3,8 @@
 <#
 Get-ProcDump
 Acquires a Sysinternal procdump of the specified process
-Requires prodcump.exe be in $env:SystemRoot
-This is really more of a PoC for how one might collect binary
-data, like a memory dump, using Kansa.
 
-!! SCRIPT ASSUMES PROCDUMP IS IN $ENV:SYSTEMROOT!!
-
-If you have procdump.exe in your Modules\bin\ path and run the script with the -Pushbin
+If you have procdump.exe in your Modules\bin\ path and run Kansa with the -Pushbin
 flag, Kansa will attempt to copy the binary to the ADMIN$. Binaries are not removed, so
 subsequent runs won't require -Pushbin.
 
@@ -18,8 +13,11 @@ interested in. By default it dumps itself, which is probably
 not what you want.
 #>
 
+# Replace $pid with the process id you wish to capture.
+$ProcId = $pid
+
 if (Test-Path "$env:SystemRoot\Procdump.exe") {
-    $PDOutput = & $env:SystemRoot\Procdump.exe /accepteula $pid 2> $null
+    $PDOutput = & $env:SystemRoot\Procdump.exe /accepteula $ProcId 2> $null
     foreach($line in $PDOutput) {
         if ($line -match 'Writing dump file (.*\.dmp) ...') {
             Get-Content -ReadCount 0 -Raw -Encoding Byte $($matches[1])

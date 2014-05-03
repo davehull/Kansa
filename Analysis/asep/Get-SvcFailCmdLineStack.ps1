93bc60a71983f736ab4d8d949765c8fc97e58ce6
Dave Hull <dpgit@trustedsignal.com>
2014-05-03 01:13:43 -0700

    Added a simple stacker for Service Failures. Modified Get-SvcTrigs.ps1 to use Microsoft's terms.

---
+++ Analysis/asep/Get-SvcFailCmdLineStack.ps1
@@ -0,0 +1,30 @@
+﻿<#
+Get-SvcFailCmdLineStack.ps1
+Requires logparser.exe in path
+Pulls stack rank of Service Failure CmdLines from acquired Service Failure data
+where CmdLine is not 'customScript.cmd' nor NULL
+
+This script expects files matching the pattern *SvcFail.tsv to be in 
+the current working directory.
+#>
+
+if (Get-Command logparser.exe) {
+
+    $lpquery = @"
+    SELECT
+        COUNT(To_Lowercase(CmdLine)) as ct, 
+        To_Lowercase(CmdLine) as CmdLineLC
+    FROM
+        *SvcFail.tsv
+    GROUP BY
+        CmdLineLC
+    ORDER BY
+        ct ASC
+"@
+
+    & logparser -i:tsv -fixedsep:on -dtlines:0 -rtp:50 $lpquery
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}
--- Modules/Get-SvcTrigs.ps1
+++ Modules/Get-SvcTrigs.ps1
@@ -2,7 +2,7 @@
 <#
 Acquires service triggers
 #>
-$data = $($(foreach ($svc in (& $env:windir\system32\sc.exe query)) { 
+$svctrigs = $($(foreach ($svc in (& $env:windir\system32\sc.exe query)) { 
     if ($svc -match "SERVICE_NAME:\s(.*)") {
         & $env:windir\system32\sc qtriggerinfo $($matches[1])
     }
@@ -27,28 +27,28 @@ function Get-LogProviderHash {
 }
 
 $LogProviders = Get-LogProviderHash
-$ServiceName = $Action = $Condition = $Value = $False
+$ServiceName = $Action = $Condition = $Data = $False
 $CondPattern = '(?<Desc>[-_ A-Za-z0-9]+)\s:\s(?<Guid>[-0-9a-fA-F]+)\s(?<Trailer>[-\[A-Za-z0-9 ]+\]*)'
-foreach($line in $data) {
+foreach($line in $svctrigs) {
     $line = $line.Trim()
     if ($line -match "SERVICE_NAME:\s(?<SvcName>[-_A-Za-z0-9]+)") {
         if ($ServiceName) {
-            $o = "" | Select-Object ServiceName, Action, Condition, Value
-            $o.ServiceName, $o.Action, $o.Condition, $o.Value = (($ServiceName,$Action,$Condition,$Value) -replace "False", $null)
+            $o = "" | Select-Object ServiceName, Action, Type, Subtype, Data
+            $o.ServiceName, $o.Action, $o.Type, $o.Subtype, $o.Data = (($ServiceName,$Action,$Type,$SubType,$Data) -replace "False", $null)
             $o
         }
         $ServiceName = $matches['SvcName']
-        $Action = $Condition = $Value = $False
+        $Action = $Condition = $Data = $False
     } elseif ($line -match "(START SERVICE|STOP SERVICE)") {
         if ($ServiceName -and $Action -and $Condition) {
-            $o = "" | Select-Object ServiceName, Action, Condition, Value
-            $o.ServiceName, $o.Action, $o.Condition, $o.Value = (($ServiceName,$Action,$Condition,$Value) -replace "False", $null)
+            $o = "" | Select-Object ServiceName, Action, Type, Subtype, Data
+            $o.ServiceName, $o.Action, $o.Type, $o.Subtype, $o.Data = (($ServiceName,$Action,$Type,$Subtype,$Data) -replace "False", $null)
             $o
         }
         $Action = ($matches[1])
-        $Condition = $Value = $False
+        $Condition = $Data = $False
     } elseif ($line -match "DATA\s+") {
-        $Value = $line -replace "\s+"
+        $Data = $line -replace "\s+" -replace "DATA:"
     } else {
         $Condition = $line -replace "\s+", " "
         if ($LogProviders) {
@@ -57,13 +57,17 @@ foreach($line in $data) {
                 $Guid = $($matches['Guid']).ToUpper()
                 $ProviderName = $LogProviders.$Guid
                 if ($ProviderName) {
-                    $Condition = ($matches['Desc'] + ": " + $LogProviders.$Guid) #+ " " + $matches['Trailer'])
+                    $Type = $matches['Desc']
+                    $Subtype = $LogProviders.$Guid
+                } else {
+                    $Type, $Subtype = ($Condition -split ":")
+                    $Subtype = $Subtype.Trim()
                 }
             }
         }
-        $Value = $False
+        $Data = $False
     }
 }
-$o = "" | Select-Object ServiceName, Action, Condition, Value
-$o.ServiceName, $o.Action, $o.Condition, $o.Value = (($ServiceName,$Action,$Condition,$Value) -replace "False", $null)
+$o = "" | Select-Object ServiceName, Action, Type, Subtype, Data
+$o.ServiceName, $o.Action, $o.Type, $o.Subtype, $o.Data = (($ServiceName,$Action,$Type,$Subtype,$Data) -replace "False", $null)
 $o

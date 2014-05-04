a1247b6d9a15faa9f801fab57aa382ba6a8712f6
Dave Hull <dpgit@trustedsignal.com>
2014-05-04 09:54:36 -0700

    Commiting a DNSCache Stacker.

---
+++ Analysis/network/Get-DNSCacheStack.ps1
@@ -0,0 +1,29 @@
+﻿<#
+Get-DNSCacheStack.ps1
+Requires logparser.exe in path
+Pulls frequency of DNSCache entries
+
+This script expects files matching the *DNSCache.tsv pattern to be in the
+current working directory.
+#>
+
+
+if (Get-Command logparser.exe) {
+    $lpquery = @"
+    SELECT
+        COUNT(FQDN) as ct,
+        FQDN
+    FROM
+        *DNSCache.tsv
+    GROUP BY
+        FQDN
+    ORDER BY
+        ct ASC
+"@
+
+    & logparser -i:tsv -dtlines:0 -rtp:50 "$lpquery"
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

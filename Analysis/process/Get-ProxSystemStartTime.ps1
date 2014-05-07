46ca0f6fda24f3b420a4a337cfefba032c1e3af2
Dave Hull <dpgit@trustedsignal.com>
2014-05-06 20:01:47 -0700

    Added Get-ProxSystemStartTime.ps1 for pulling System process start time from Prox data, this is practically the system's boot time.

---
+++ Analysis/process/Get-ProxSystemStartTime.ps1
@@ -0,0 +1,14 @@
+﻿<#
+Get-SystemStartTime.ps1
+
+Pulls the StartTime for the System process for each host,
+sort the data for all hosts.
+
+Process data matching *Prox.xml in pwd
+#>
+
+$(foreach($file in (ls *Prox.xml)) {
+    $data = Import-Clixml $file
+    $data | Where-Object { $_.ProcessName -eq "System" } |
+        Select-Object PSComputerName, ProcessName, StartTime
+}) | Sort-Object StartTime

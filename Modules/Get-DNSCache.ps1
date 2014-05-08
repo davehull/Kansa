e04857a0c3738e6e8dbe02c889581309122470bd
Dave Hull <dpgit@trustedsignal.com>
2014-05-07 20:54:05 -0700

    Updating Get-DNSCache.ps1 to use the Powershell commandlet if available. It provides richer data.

--- Modules/Get-DNSCache.ps1
+++ Modules/Get-DNSCache.ps1
@@ -4,9 +4,13 @@
 Get-DNSCache.ps1 acquires DNS cache entries from the target host.
 #>
 
-& ipconfig /displaydns | Select-String 'Record Name' | ForEach-Object { $_.ToString().Split(' ')[-1] } | `
-  Select-Object -Unique | sort | % {
-    $o = "" | Select-Object FQDN
-    $o.FQDN = $_
-    $o
-  }
+if (Get-Command Get-DnsClientCache) {
+    Get-DnsClientCache
+} else {
+    & ipconfig /displaydns | Select-String 'Record Name' | ForEach-Object { $_.ToString().Split(' ')[-1] } | `
+      Select-Object -Unique | sort | % {
+        $o = "" | Select-Object FQDN
+        $o.FQDN = $_
+        $o
+    }
+}

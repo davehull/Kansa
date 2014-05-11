b8144f0241464fbf68fc896c0a2105c160ba8ea9
Dave Hull <dpgit@trustedsignal.com>
2014-05-10 23:06:29 -0700

    Renamed Get-tlistv.ps1 to Get-Tasklistv.ps1 and modified output to be PS Objects.

---
+++ Modules/Get-Tasklistv.ps1
@@ -0,0 +1,19 @@
+﻿# OUTPUT tsv
+<#
+tlistv acquires tasklist /v (verbose output includes process owner) and formats as csv
+#>
+$data = & $env:windir\system32\tasklist.exe /v /fo csv
+$data | Select-Object -Skip 1 | % {
+    $o = "" | Select-Object ImageName,PID,SessionName,SessionNum,MemUsage,Status,UserName,CPUTime,WindowTitle
+    $row = $_ -replace '(,)(?=(?:[^"]|"[^"]*")*$)', "`t" -replace "`""
+    $o.ImageName, 
+    $o.PID,
+    $o.SessionName,
+    $o.SessionNum,
+    $o.MemUsage,
+    $o.Status,
+    $o.UserName,
+    $o.CPUTime,
+    $o.WindowTitle = ( $row -split "`t")
+    $o
+}
--- Modules/Get-tlistv.ps1
+++
@@ -1,5 +0,0 @@
-﻿# OUTPUT txt
-<#
-tlistv acquires tasklist /v (verbose output includes process owner) and formats as csv
-#>
-& $env:windir\system32\tasklist.exe /v /fo csv

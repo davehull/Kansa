4af964541947ad8b195478604088a30f24c24fbb
Dave Hull <dpgit@trustedsignal.com>
2014-05-11 18:52:42 -0700

    Get-SvcAll.ps1 now returns PS objects, tells Kansa to save data as tsv.

--- Modules/Get-SvcAll.ps1
+++ Modules/Get-SvcAll.ps1
@@ -1,7 +1,19 @@
-﻿# OUTPUT xml
+﻿# OUTPUT tsv
 <#
-Get-AllServices.ps1
-Acquires details about running and auto-start services
+.SYNOPSIS
+Get-SvcAll.ps1
+Returns Service's Name, DisplayName, PathName, StartName, StartMode, State, TotalSessions, Description
 #>
 
-Get-WmiObject win32_service
+Get-WmiObject win32_service | % {
+    $o = "" | Select-Object Name, DisplayName, PathName, StartName, StartMode, State, TotalSessions, Description
+    $o.Name          = $_.Name
+    $o.DisplayName   = $_.DisplayName
+    $o.PathName      = $_.PathName
+    $o.StartName     = $_.StartName
+    $o.StartMode     = $_.StartMode 
+    $o.State         = $_.State
+    $o.TotalSessions = $_.TotalSessions
+    $o.Description   = $_.Description
+    $o
+}

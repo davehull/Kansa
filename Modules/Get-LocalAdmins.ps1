06cc97b32c2a8657a80545356d03709ad2a146e0
Dave Hull <dpgit@trustedsignal.com>
2014-05-12 17:56:26 -0700

    Added Get-LocalAdmins.ps1 collector

---
+++ Modules/Get-LocalAdmins.ps1
@@ -0,0 +1,12 @@
+﻿# OUTPUT tsv
+<#
+.SYNOPSIS
+Returns a list of local administrators
+#>
+& net localgroup administrators | Select-Object -Skip 6 | ? {
+    $_ -and $_ -notmatch "The command completed successfully" 
+} | % {
+    $o = "" | Select-Object Account
+    $o.Account = $_
+    $o
+}

40e0e851797d2ef92348b585671d2e07922925c5
Dave Hull <dpgit@trustedsignal.com>
2014-05-20 22:12:35 -0700

    Added Get-CertStore.ps1 collector for dumping certificates from the cert store. Added some things to the ToDo list.

---
+++ Modules/Get-CertStore.ps1
@@ -0,0 +1,19 @@
+﻿# OUTPUT txt
+# BINDEP sigcheck.exe
+<#
+.SYNOPSIS
+Get-CertStore.ps1
+Uses Sysinternals sigcheck.exe to dump the contents of remote system's certificate
+stores.
+
+Requires sigcheck.exe from Sysinternals. If you copy it to to the Modules\bin\ directory
+and run Kansa with the -Pushbin option the sigcheck binary will be copied to remote
+host's ADMIN$ shares. Alternatively, you can pre-install sigcheck.exe in the ADMIN$
+share of targets and run without the -Pushbin argument.
+#>
+
+if (Test-Path "$env:SystemRoot\sigcheck.exe") {
+    & $env:SystemRoot\sigcheck.exe -q -t '*'
+} else {
+    "sigcheck.exe not found on $env:COMPUTERNAME"
+}    

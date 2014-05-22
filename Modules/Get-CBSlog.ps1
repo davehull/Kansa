60489efda77a921259aef5195b11a0887ce5cbd8
Dave Hull <dpgit@trustedsignal.com>
2014-05-21 20:37:37 -0700

    Get-CBSlog.ps1 collector. Updated Todo to reflect new analysis needs.

---
+++ Modules/Get-CBSlog.ps1
@@ -0,0 +1,13 @@
+﻿# OUTPUT TSV
+# Returns the contents of the CBS log.
+
+Get-Content $env:windir\logs\CBS\cbs.log | % { 
+    $_ -replace "\s\s+", "`t" 
+} | % {
+    $r = [regex]','
+    $r.replace($_, "`t", 1)
+} | % {
+    $o = "" | Select-Object Timestamp, MessageType, LogSource, Message
+    $o.Timestamp, $o.MessageType, $o.LogSource, $o.Message = ($_ -split "`t").Trim()
+    $o
+}

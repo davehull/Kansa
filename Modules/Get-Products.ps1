3c01c197b429736ffdf24045586a9c0c3d2eae3b
Dave Hull <dpgit@trustedsignal.com>
2014-05-22 17:14:18 -0700

    Adding a Get-Products collector, returns list of software installed via MS installer.

---
+++ Modules/Get-Products.ps1
@@ -0,0 +1,8 @@
+﻿# OUTPUT tsv
+<#
+.SYNOPSIS
+Get-Products.ps1
+Returns objects for products installed via Windows Installer, not likely to be malware for sure, but
+may give an indication of vulnerable product versions.
+#>
+Get-WmiObject Win32_Product

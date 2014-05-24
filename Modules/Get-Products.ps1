f94be7726e50022589c0eb5bb9a1f130e6accfec
Dave Hull <dpgit@trustedsignal.com>
2014-05-23 17:59:37 -0700

    Added Get-Hotfix.ps1.

--- Modules/Get-Products.ps1
+++ Modules/Get-Products.ps1
@@ -2,7 +2,7 @@
 <#
 .SYNOPSIS
 Get-Products.ps1
-Returns objects for products installed via Windows Installer, not likely to be malware for sure, but
-may give an indication of vulnerable product versions.
+Returns objects for products installed via Windows Installer, not likely 
+to be malware for sure, but may give an indication of vulnerable product versions.
 #>
 Get-WmiObject Win32_Product

a410d339c03a2c3c55974c6eabb2a0243ce76d01
Dave Hull <dpgit@trustedsignal.com>
2014-05-27 21:00:14 -0700

    Adding a couple collectors for SMB Shares and Sessions. Modified Modules.conf accordingly.

---
+++ Modules/Config/Get-SmbShare.ps1
@@ -0,0 +1,5 @@
+﻿# OUTPUT TSV
+# Returns share info for this host
+if (Get-Command Get-SmbShare -ErrorAction SilentlyContinue) {
+    Get-SmbShare
+}
---
+++ Modules/Net/Get-SmbSession.ps1
@@ -0,0 +1,5 @@
+﻿# OUTPUT TSV
+# Returns Smb sessions to this host
+if (Get-Command Get-SmbSession -ErrorAction SilentlyContinue) {
+    Get-SmbSession
+}

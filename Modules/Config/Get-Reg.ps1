e7b9ec1a69422a85179396adb09dca5156a3fce5
Dave Hull <dpgit@trustedsignal.com>
2014-05-28 21:12:39 -0700

    Working on new Registry collector.

---
+++ Modules/Config/Get-Reg.ps1
@@ -0,0 +1,14 @@
+﻿# OUTPUT txt
+# Get-Reg.ps1 is the start of a series of modules that will collect registry based artifacts
+
+if ($pathToReg = Get-Command Reg.exe | Select-Object -ExpandProperty path) { 
+    foreach ($userpath in (Get-WmiObject win32_userprofile | Select-Object localpath)) {
+    <# TKTK if $userpath contains an ntuser.dat
+    reg load "hku\$userpath" "$userpath\ntuser.dat"
+    set-location registry::\hkey_users
+    get-itemproperty .\$userpath\path\to\key\value
+    
+
+    #>
+    }
+}

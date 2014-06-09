840aa1aa1a0a257959d14fec6b2625453fbd4ad9
Dave Hull <dpgit@trustedsignal.com>
2014-06-09 11:02:15 -0700

    Added Get-ProcsWMICLIStack.ps1 for frequency analysis of ProcsWMI data.

---
+++ Analysis/process/Get-ProcsWMICLIStack.ps1
@@ -0,0 +1,33 @@
+﻿<#
+Get-ProcsWMICLIStack.ps1
+
+Returns frequency of "CommandLine," which is
+ExecutablePath and command line arguments and hash
+of file on disk
+
+Requires:
+Process data matching *ProcWMI.tsv in pwd
+logparser.exe in path
+#>
+
+if (Get-Command logparser.exe) {
+    $lpquery = @"
+    SELECT
+        COUNT(CommandLine) as Cnt,
+        CommandLine,
+        Hash
+    FROM
+        *ProcsWMI.tsv
+    GROUP BY
+        CommandLine,
+        Hash
+    ORDER BY
+        Cnt ASC
+"@
+
+    & logparser -i:tsv -dtlines:0 -fixedsep:on -rtp:50 "$lpquery"
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

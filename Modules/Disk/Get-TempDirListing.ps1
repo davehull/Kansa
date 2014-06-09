4fbfbfea1b26d38122d49bc437ab939e84d3b5da
Dave Hull <dpgit@trustedsignal.com>
2014-06-09 08:04:54 -0700

    Added collector for Temp directory listings.

---
+++ Modules/Disk/Get-TempDirListing.ps1
@@ -0,0 +1,11 @@
+﻿# OUTPUT tsv
+<#
+.SYNOPSIS
+Get listing of common temporary directories
+#>
+
+foreach($userpath in (Get-WmiObject win32_userprofile | Select-Object -ExpandProperty localpath)) {
+    if (Test-Path(($userpath + "\AppData\Local\Temp\"))) {
+        Get-ChildItem ($userpath + "\AppData\Local\Temp\*") | Select-Object FullName, CreationTimeUtc, LastAccessTimeUtc, LastWriteTimeUtc
+    }
+}

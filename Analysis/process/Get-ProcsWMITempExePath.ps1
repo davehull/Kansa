e2fdfcf9a58a37f4ada6321d757649b2ba4b092e
Dave Hull <dpgit@trustedsignal.com>
2014-06-09 09:10:20 -0700

    Adding analysis script to look for processes running from common temporary paths.

---
+++ Analysis/process/Get-ProcsWMITempExePath.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Get-ProcsWMITempExePath.ps1
+
+Returns DISTINCT process CreationDate, ProcessId, ParentProcessId, CommandLine, 
+PSComputerName for any processes with ExecutablePaths containing Temp, Tmp or 
+AppData\Local, common temporary folders
+
+Requires:
+Process data matching *ProcWMI.tsv in pwd
+logparser.exe in path
+#>
+
+if (Get-Command logparser.exe) {
+    $lpquery = @"
+    SELECT DISTINCT
+        CreationDate,
+        ProcessId,
+        ParentProcessId,
+        ExecutablePath,
+        CommandLine,
+        PSComputerName
+    FROM
+        *ProcsWMI.tsv
+    WHERE
+        ExecutablePath like '%Temp%' or
+        ExecutablePath like '%Tmp%' or
+        ExecutablePath like '%AppData\Local%'
+    ORDER BY
+        PSComputerName,
+        CreationDate,
+        ProcessId ASC
+"@
+
+    & logparser -i:tsv -dtlines:0 -fixedsep:on -rtp:50 "$lpquery"
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

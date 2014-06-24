9602443455add4459fe663640c08a4fd821c596e
Dave Hull <dpgit@trustedsignal.com>
2014-06-24 07:32:02 -0700

    Adding files after renaming paths.

---
+++ Analysis/Net/Get-ARPStack.ps1
@@ -0,0 +1,33 @@
+﻿<#
+Get-ARPStack.ps1
+Requires logparser.exe in path
+Pulls frequency of ARP based on IpAddr
+
+This script expects files matching the *Arp.tsv pattern to be in the
+current working directory.
+#>
+
+
+if (Get-Command logparser.exe) {
+    $lpquery = @"
+    SELECT
+        COUNT(IpAddr, Mac, Type) as ct,
+        IpAddr,
+        Mac,
+        Type
+    FROM
+        *arp.tsv
+    GROUP BY
+        IpAddr,
+        Mac,
+        Type
+    ORDER BY
+        ct ASC
+"@
+
+    & logparser -i:tsv -dtlines:0 -fixedsep:on -rtp:-1 "$lpquery"
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}
---
+++ Analysis/Net/Get-NetstatStackForeignIpProcess.ps1
@@ -0,0 +1,56 @@
+﻿<#
+Get-NetstatStackForeignIpProcess.ps1
+Requires logparser.exe in path
+
+Pulls stack rank of connections from netstat data based on 
+ForeignAddress and Process. 
+
+Assumes local network space is using 10/8 CIDR and looks for
+foreign addresses outside that range. See query for additional
+filtered IP ranges and processes.
+
+You may want to customize this a bit for
+your environment. If you're running web 
+servers, for example, stacking them may be
+meaningless if they are meant to accept 
+connections from any host in the world, or
+maybe you just remove the web server process
+from the query...
+
+!! YOU WILL LIKELY WANT TO ADJUST THIS QUERY !!
+
+This script exepcts files matching the pattern 
+*netstat.tsv to be in the current working
+directory
+#>
+
+if (Get-Command logparser.exe) {
+
+    $lpquery = @"
+    SELECT
+        COUNT(ForeignAddress,
+        Process) as ct,
+        ForeignAddress,
+        Process
+    FROM
+        *netstat.tsv
+    WHERE
+        ConPid not in ('0'; '4') and
+        ForeignAddress not like '10.%' and
+        ForeignAddress not like '169.254%' and
+        ForeignAddress not in ('*'; '0.0.0.0'; 
+            '127.0.0.1'; '[::]'; '[::1]')
+    GROUP BY
+        ForeignAddress,
+        Process
+    ORDER BY
+        Process,
+        ct desc
+"@
+
+    & logparser -i:tsv -fixedsep:on -dtlines:0 -rtp:-1 $lpquery
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

c562ccfdac4f5e95cff6f34039d631704f0dd4be
Dave Hull <dpgit@trustedsignal.com>
2014-07-02 21:43:19 -0700

    Updated Get-ProcDump.ps1 to take a process id as a parameter.

--- Modules/Process/Get-ProcDump.ps1
+++ Modules/Process/Get-ProcDump.ps1
@@ -1,20 +1,35 @@
 ﻿# OUTPUT bin
 # BINDEP .\Modules\bin\Procdump.exe
 <#
-Get-ProcDump
-Acquires a Sysinternal procdump of the specified process
+.SYNOPSIS
+Get-ProcDump.ps1 acquires a Sysinternal procdump of the specified 
+process
+.PARAMETER ProcId
+A required parameter, the process id of the process you want to dump.
+.NOTES
+When used with Kansa.ps1, parameters must be positional. Named params
+are not supported.
+.EXAMPLE
+Get-ProcDump.ps1 104
 
-If you have procdump.exe in your Modules\bin\ path and run Kansa with the -Pushbin
-flag, Kansa will attempt to copy the binary to the ADMIN$. Binaries are not removed, so
-subsequent runs won't require -Pushbin.
+When passing specific modules with parameters via Kansa.ps1's
+-ModulePath parameter, be sure to quote the entire string, like 
+shown here:
+.\kansa.ps1 -Target localhost -ModulePath ".\Modules\Process\Get-ProcDumpe.ps1 104"
 
-Also, you should configure this to dump the process you're 
-interested in. By default it dumps itself, which is probably
-not what you want.
+If you have procdump.exe in your Modules\bin\ path and run Kansa with 
+the -Pushbin flag, Kansa will attempt to copy the binary to the ADMIN$.
+Binaries are not removed, so subsequent runs won't require -Pushbin.
+
+Also, you should configure this to dump the process you're interested
+in. By default it dumps itself, which is probably not what you want.
 #>
 
-# Replace $pid with the process id you wish to capture.
-$ProcId = $pid
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$False,Position=0)]
+        [Int]$ProcId=$pid
+)
 
 if (Test-Path "$env:SystemRoot\Procdump.exe") {
     $PDOutput = & $env:SystemRoot\Procdump.exe /accepteula $ProcId 2> $null

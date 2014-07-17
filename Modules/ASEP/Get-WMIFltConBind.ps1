f2c965e32bf709211e9733b12c1b32949cd356d8
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 07:49:29 -0700

    Updated for new directive format.

--- Modules/ASEP/Get-WMIFltConBind.ps1
+++ Modules/ASEP/Get-WMIFltConBind.ps1
@@ -1,2 +1,11 @@
-﻿# OUTPUT TSV
+﻿<#
+.SYNOPSIS
+Get-WMIFiltConBind.ps1 returns information about the WMI Event Filter
+to Consumer binding, a necessary component for triggering WMI Event
+Consumers, which have been used by malware as a persistence mechanism
+.NOTES
+The following line is an OUPUT directive used by kansa.ps1 to determine
+how this scripts output should be handled.
+OUTPUT TSV
+#>
 Get-WmiObject -Namespace root\subscription -Query "select * from __FilterToConsumerBinding"

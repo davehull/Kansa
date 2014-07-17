29f9e5863e180a79cb806d02c524eeff3685ec0c
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 09:13:31 -0700

    Updated for new directive format.

--- Modules/Config/Get-SmbShare.ps1
+++ Modules/Config/Get-SmbShare.ps1
@@ -1,5 +1,13 @@
-﻿# OUTPUT TSV
-# Returns share info for this host
+﻿<#
+.SYNOPSIS
+Get-SmbShare.ps1 returns information about SMB Shares, which could be
+created by adversaries for data collection points.
+.NOTES
+The next line is required by Kansa.ps1, it tells Kansa how to handle 
+the output returned.
+OUTPUT TSV
+#>
+
 if (Get-Command Get-SmbShare -ErrorAction SilentlyContinue) {
     Get-SmbShare
 }

4663c4c6819788e5f3f66b5fb928043e7c7011e3
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 16:39:10 -0700

    Directive change.

--- Modules/Net/Get-NetIPInterfaces.ps1
+++ Modules/Net/Get-NetIPInterfaces.ps1
@@ -1,6 +1,9 @@
-﻿# OUTPUT tsv
-<#
+﻿<#
+.SYNOPSIS
 Get-NetIPInterfaces.ps1
 Returns data from Get-NetIPInterface
+.NOTES
+Next line tells Kansa.ps1 how to format this script's output
+OUTPUT tsv
 #>
 Get-NetIPInterface

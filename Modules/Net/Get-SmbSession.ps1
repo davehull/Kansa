477bc0e401dfe35cf58cf795ebc703edc37159f1
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 16:35:41 -0700

    Directive change.

--- Modules/Net/Get-SmbSession.ps1
+++ Modules/Net/Get-SmbSession.ps1
@@ -1,5 +1,12 @@
-﻿# OUTPUT TSV
-# Returns Smb sessions to this host
+﻿<#
+.SYNOPSIS
+Get-SmbSession.ps1 returns smb sessions connected to this host.
+
+.NOTES
+Next line needed by Kansa.ps1 for handling this scripts output.
+OUTPUT TSV
+#>
+
 if (Get-Command Get-SmbSession -ErrorAction SilentlyContinue) {
     Get-SmbSession
 }

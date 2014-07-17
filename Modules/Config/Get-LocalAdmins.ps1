f4b189a8bc69f443496ce926217a2ba24d750932
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 10:21:55 -0700

    Updated for new directive.

--- Modules/Config/Get-LocalAdmins.ps1
+++ Modules/Config/Get-LocalAdmins.ps1
@@ -1,5 +1,12 @@
-﻿# OUTPUT tsv
-# Returns a list of local administrators
+﻿<#
+.SYNOPSIS
+Get-LocalAdmins.ps1 returns a list of local administrator accounts.
+.NOTES
+Next line is required by Kansa.ps1. It instructs Kansa how to handle
+the output from this script.
+OUTPUT tsv
+#>
+
 & net localgroup administrators | Select-Object -Skip 6 | ? {
     $_ -and $_ -notmatch "The command completed successfully" 
 } | % {

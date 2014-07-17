5e7fe2c9f4c77cd93364379b52d89f203496764e
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 10:20:16 -0700

    Updated for new directive.

--- Modules/Config/Get-Products.ps1
+++ Modules/Config/Get-Products.ps1
@@ -1,8 +1,10 @@
-﻿# OUTPUT tsv
-<#
+﻿<#
 .SYNOPSIS
-Get-Products.ps1
-Returns objects for products installed via Windows Installer, not likely 
-to be malware for sure, but may give an indication of vulnerable product versions.
+Get-Products.ps1 returns data about products that were installed via 
+the Windows installer.
+.NOTES
+The next line is needed by Kansa.ps1 to determine how the output from
+this script should be handled.
+OUTPUT tsv
 #>
 Get-WmiObject Win32_Product

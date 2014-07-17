190c4caf2dbcfa336254c59cc34fafe15c4fbcb5
Dave Hull <dpgit@trustedsignal.com>
2014-07-17 10:45:32 -0700

    Directives.

--- Modules/Config/Get-CertStore.ps1
+++ Modules/Config/Get-CertStore.ps1
@@ -1,15 +1,28 @@
-﻿# OUTPUT txt
-# BINDEP .\Modules\bin\sigcheck.exe
-<#
+﻿<#
 .SYNOPSIS
-Get-CertStore.ps1
-Uses Sysinternals sigcheck.exe to dump the contents of remote system's certificate
-stores.
+Get-CertStore.ps1 uses Sysinternals' sigcheck.exe to acquire data about
+hosts' certificate stores.
 
-Requires sigcheck.exe from Sysinternals. If you copy it to to the Modules\bin\ directory
-and run Kansa with the -Pushbin option the sigcheck binary will be copied to remote
-host's ADMIN$ shares. Alternatively, you can pre-install sigcheck.exe in the ADMIN$
-share of targets and run without the -Pushbin argument.
+!! Get-CertStore.ps1 requires Sysinternals' sigcheck.exe to be in the 
+ADMIN$ share of remote hosts !!
+
+Sigcheck.exe is not packaged with Kansa. You must download it from 
+Sysinternals and drop it in .\Modules\bin\, then if you run Kansa.ps1 
+with the -Pushbin flag, it will attempt to copy sigcheck.exe to the 
+ADMIN$ share of remote hosts (targets).
+
+Running Kansa.ps1 with the -rmbin flag will cause Kansa to attempt to
+remove binaries that were copied to remote hosts via -Pushbin.
+Alternatively, you can leave the binaries on remote hosts and future
+Kansa runs may not require the -Pushbin flag.
+
+.NOTES
+The following lines are required by Kansa.ps1. The first is an OUTPUT
+directive that tells Kansa how to tread the output from this script.
+The second tells Kansa where to find the sigcheck binary so it can copy
+it to targets when run with the -Pushbin flag.
+OUTPUT txt
+BINDEP .\Modules\bin\sigcheck.exe
 #>
 
 if (Test-Path "$env:SystemRoot\sigcheck.exe") {

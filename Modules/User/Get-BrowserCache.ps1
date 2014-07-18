9851ac1ee027e2b21ca94ddf9dc90d188c7fa926
z4ns4tsu <z4ns4tsu@outlook.com>
2014-07-18 14:29:29 -0700

    Adding some more bones to \Modules\User\Get-BrowserCache.ps1

--- Modules/User/Get-BrowserCache.ps1
+++ Modules/User/Get-BrowserCache.ps1
@@ -17,9 +17,64 @@ parameter, be sure to quote the entire string, like shown here:
 [CmdletBinding()]
 Param(
 	[Parameter(Mandatory=$False,Position=0)]
-		[ValidateSet("FireFox","Chrome","InternetExplorer","Auto")]
+		[ValidateSet("Auto","Chrome","InternetExplorer","FireFox")]
 		[string]$BrowserFamily="Auto",
 	[Parameter(Mandatory=$False,Position=1)]
 		[string]$UserName
 )
+
+# Hashtable of browsers we know how to parse and the name of their parse function
+$supportedBrowsers = @{
+	0x01 = "Get-IeCache";
+	0x02 = "Get-FirefoxCache";
+	0x04 = "Get-ChromeCache"
+}
+
+function Get-InstalledBrowsers {
+	Write-Verbose "Automatically determining installed browsers."
+	[int]$browsers = 0
+	$app_path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths"
+	foreach ($entry in (Get-ChildItem $app_path | Select-Object -ExpandProperty Name -ErrorAction SilentlyContinue))
+	{
+		$entry = $entry -replace "HKEY_LOCAL_MACHINE", "HKLM:"
+		$supress = (Get-ItemProperty $entry | Select-Object -ExpandProperty PSChildName -ErrorAction SilentlyContinue)
+		if ($supress -match "IEXPLORE.EXE") 
+		{
+			Write-Verbose "Found Internet Explorer."
+			$browsers += 1 
+		}
+		elseif ($supress -match "firefox.exe") 
+		{ 
+			Write-Verbose  "Found Firefox."
+			$browsers += 2 
+		}
+		elseif ($supress -match "chrome.exe") 
+		{ 
+			Write-Verbose "Found Chrome."
+			$browsers += 4 
+		}
+	}
+
+	return $browsers
+}
+
+function Get-IeCache {
+
+}
+
+function Get-FirefoxCache {
+
+}
+
+function Get-ChromeCache {
+
+}
+
+if ($BrowserFamily -eq "Auto")
+{
+	$browsers = Get-InstalledBrowsers 
+	$browsers
+
+	$supportedBrowsers.Keys | ? { $_-band $browsers } | % { $supportedBrowsers.Get_Item($_) }
+}
 

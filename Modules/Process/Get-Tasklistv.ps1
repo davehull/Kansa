1b4f01a01e0d2ab286dc729863c86498182ec5ec
Dave Hull <dpgit@trustedsignal.com>
2014-07-18 07:35:27 -0700

    Directive update

--- Modules/Process/Get-Tasklistv.ps1
+++ Modules/Process/Get-Tasklistv.ps1
@@ -1,6 +1,9 @@
-﻿# OUTPUT tsv
-<#
+﻿<#
+.SYNOPSIS
 Get-Tasklistv.ps1 acquires tasklist /v (verbose output includes process owner) and formats as csv
+.NOTES
+Kansa.ps1 output directive follows
+OUTPUT tsv
 #>
 & $env:windir\system32\tasklist.exe /v /fo csv | Select-Object -Skip 1 | % {
     $o = "" | Select-Object ImageName,PID,SessionName,SessionNum,MemUsage,Status,UserName,CPUTime,WindowTitle

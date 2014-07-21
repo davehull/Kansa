5a247f265c651c2ee8278ce57fb6315e63458920
Dave Hull <dpgit@trustedsignal.com>
2014-07-20 20:52:53 -0700

    Updated for new directive format

--- Analysis/asep/Get-ASEPImagePathLaunchStringStack.ps1
+++ Analysis/asep/Get-ASEPImagePathLaunchStringStack.ps1
@@ -1,5 +1,5 @@
-﻿# DATADIR Autorunsc
-<#
+﻿<#
+.SYNOPSIS
 Get-ASEPImagePathLaunchStringStack.ps1
 Requires logparser.exe in path
 Pulls frequency of autoruns based on ImagePath, LaunchString tuple
@@ -7,6 +7,8 @@ where ImagePath is not 'File not found'
 
 This script expects files matching the pattern *autorunsc.txt to be in the
 current working directory.
+.NOTES
+DATADIR Autorunsc
 #>
 
 if (Get-Command logparser.exe) {

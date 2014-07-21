398836d9aa4e8796639b5d0a975a773e48230e08
Dave Hull <dpgit@trustedsignal.com>
2014-07-21 07:59:16 -0700

    Directive.

--- Analysis/process/Get-ProcsWMICLIMD5Stack.ps1
+++ Analysis/process/Get-ProcsWMICLIMD5Stack.ps1
@@ -1,5 +1,5 @@
-﻿# DATADIR ProcsWMI
-<#
+﻿<#
+.SYNOPSIS
 Get-ProcsWMICLIStack.ps1
 
 Returns frequency of "CommandLine," which is
@@ -9,6 +9,8 @@ of file on disk
 Requires:
 Process data matching *ProcWMI.tsv in pwd
 logparser.exe in path
+.NOTES
+DATADIR ProcsWMI
 #>
 
 if (Get-Command logparser.exe) {

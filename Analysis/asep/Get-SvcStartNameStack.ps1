c6437f0ae0070314a82f2442953916dece69f47a
Dave Hull <dpgit@trustedsignal.com>
2014-07-20 20:42:37 -0700

    Updated for new directive format

--- Analysis/asep/Get-SvcStartNameStack.ps1
+++ Analysis/asep/Get-SvcStartNameStack.ps1
@@ -1,5 +1,5 @@
-﻿# DATADIR SvcAll
-<#
+﻿<#
+.SYNOPSIS
 Get-SvcStartNameStack.ps1
 A basic stack for services aggregating on Caption and StartName.
 StartName tells you what account the service runs under.
@@ -13,6 +13,8 @@ the Pathnames are the same for both of them.
 
 Get-Autorunsc.ps1 output provides much of this same information,
 but it doesn't tell you the account a service will run under.
+.NOTES
+DATADIR SvcAll
 #>
 
 $data = $null

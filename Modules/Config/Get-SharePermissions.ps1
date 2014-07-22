66c9f564dd086379623f1d2d94f3c74f31da5aad
z4ns4tsu <z4ns4tsu@outlook.com>
2014-07-22 16:11:36 -0700

    Adding new module Get-SharePermissions.ps1.

---
+++ Modules/Config/Get-SharePermissions.ps1
@@ -0,0 +1,41 @@
+﻿<#
+
+#>
+
+function Get-SddlParts {
+    Param(
+        [Parameter(Mandatory=$True,Position=0)]
+            [string]$Sddl
+    )
+    
+    $splitStr = '(O:|G:|D:|S:)'
+    $splitSddl = $Sddl -split $splitStr
+
+    # Pop the first element off, as it's an empty string
+    $null, $splitSddl = $splitSddl
+
+    # Build and return an object with the different parts.
+    while ($splitSddl) {
+        $o = "" | Select-Object Part, Value
+        $key, $value, $splitSddl = $splitSddl
+        $o.Part = $key
+        $o.Value = $value
+        $o
+    }
+}
+
+
+foreach ($share in (Get-SmbShare))
+{
+    $smbPerms = Get-SmbShareAccess -Name $share.Name
+    try {
+        $aclPerms = Get-SddlParts (Get-Acl -Path $share.Path).Sddl -ErrorAction SilentlyContinue
+    }
+    catch {
+        $aclPerms = "Error getting ACL Permissions."
+    }
+
+    $share | fl *
+    $smbPerms | fl *
+    $aclPerms | fl *
+}

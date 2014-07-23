2845ce9eab180f2236accd589791efeb6bf21a53
Dave Hull <dpgit@trustedsignal.com>
2014-07-22 21:12:24 -0700

    Adding an analysis stacker on Get-ProcsWMI ProcessName field.

---
+++ Analysis/process/Get-ProcsWMIProcessNameStack.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+Get-ProcsWMIProcessNameStack.ps1
+
+Pulls frequency of processes based on path ProcessName
+
+Requires:
+Process data matching *ProcWMI.tsv in pwd
+logparser.exe in path
+.NOTES
+DATADIR ProcsWMI
+#>
+
+if (Get-Command logparser.exe) {
+    $lpquery = @"
+    SELECT
+        COUNT(ProcessName) as ct,
+        ProcessName
+    FROM
+        *ProcsWMI.tsv
+    GROUP BY
+        ProcessName
+    ORDER BY
+        ct ASC
+"@
+
+    & logparser -i:tsv -dtlines:0 -fixedsep:on -rtp:-1 "$lpquery"
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

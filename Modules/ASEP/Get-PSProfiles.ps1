3dfef697b5a6ee83bdc5a1d4a3b2f9d175e3b503
Dave Hull <dpgit@trustedsignal.com>
2014-07-30 15:40:19 -0700

    Updating for profile.ps1 profiles.

--- Modules/ASEP/Get-PSProfiles.ps1
+++ Modules/ASEP/Get-PSProfiles.ps1
@@ -2,6 +2,7 @@
 .SYNOPSIS
 Get-PSProfiles.ps1 returns copies of all Powershell profiles.
 .NOTES
+Refactor this too much duped code -- 2014-07-30 dahull
 The following line is used by Kansa.ps1 to determine how to treat the
 ouput from this script.
 OUTPUT zip
@@ -25,12 +26,19 @@ function add-zip
     }
 }
 
-
 $zipfile = (($env:TEMP) + "\" + ($env:COMPUTERNAME) + "-PSProfiles.zip")
 if (Test-Path $zipfile) { rm $zipfile -Force }
 
 foreach($path in (Get-WmiObject win32_userprofile | select -ExpandProperty LocalPath)) {
     $prfile = ($path + "\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1")
+    if (Test-Path $prfile) {
+        $thisProfile = ((Split-Path -Leaf $path) + "_" + "Microsoft.Powershell_profile.ps1")
+        $suppress = Copy-Item $prfile $env:TEMP\$thisProfile
+        ls $env:TEMP\$thisprofile | add-zip $zipfile
+        Remove-Item $env:TEMP\$thisProfile -Force
+    }
+    
+    $prfile = ($path + "\Documents\WindowsPowershell\profile.ps1")
     if (Test-Path $prfile) {
         $thisProfile = ((Split-Path -Leaf $path) + "_" + "Microsoft.Powershell_profile.ps1")
         $suppress = Copy-Item $prfile $env:TEMP\$thisProfile
@@ -47,7 +55,23 @@ if (Test-Path $alluserprofile) {
     Remove-Item $env:TEMP\$thisProfile -Force
 }
 
+$alluserprofile = ($env:windir + "\System32\WindowsPowershell\v1.0\profile.ps1")
+if (Test-Path $alluserprofile) {
+    $thisProfile = "Default_Microsoft.Powershell_profile.ps1"
+    $suppress = Copy-Item $alluserprofile $env:TEMP\$thisProfile
+    ls $env:TEMP\$thisprofile | add-zip $zipfile
+    Remove-Item $env:TEMP\$thisProfile -Force
+}
+
 $alluserprofilex86 = ($env:windir + "\SysWOW64\WindowsPowershell\v1.0\Microsoft.Powershell_profile.ps1")
+if (Test-Path $alluserprofilex86) {
+    $thisProfile = "SysWow64Default_Microsoft.Powershell_profile.ps1"
+    $suppress = Copy-Item $alluserprofilex86 $env:TEMP\$thisProfile
+    ls $env:TEMP\$thisprofile | add-zip $zipfile
+    Remove-Item $env:TEMP\$thisProfile -Force
+}
+
+$alluserprofilex86 = ($env:windir + "\SysWOW64\WindowsPowershell\v1.0\profile.ps1")
 if (Test-Path $alluserprofilex86) {
     $thisProfile = "SysWow64Default_Microsoft.Powershell_profile.ps1"
     $suppress = Copy-Item $alluserprofilex86 $env:TEMP\$thisProfile

bf8fddbb26bc6812b20dcfbe854c6db69c86563b
Dave Hull <dpgit@trustedsignal.com>
2014-08-05 10:41:39 -0700

    Adding analysis script to identify local slash 16s.

---
+++ Analysis/Net/Get-NetstatDistinctLocal16.ps1
@@ -0,0 +1,27 @@
+﻿<#
+Get-NetstatDistinctLocal16.ps1
+Requires logparser.exe in path
+Pulls distinct /16 local network addresses. Useful for building the 
+filter for local addresses in other analysis scripts, so you can see
+what hosts are communicating to hosts outside your environment.
+
+This script exepcts files matching the pattern 
+*netstat.tsv to be in the current working
+directory
+#>
+
+if (Get-Command logparser.exe) {
+
+    $lpquery = @"
+    SELECT
+        Distinct substr(LocalAddress, 0, last_index_of(substr(LocalAddress, 0, last_index_of(LocalAddress, '.')), '.')) as Local/16
+    FROM
+        *netstat.tsv
+"@
+
+    & logparser -i:tsv -fixedsep:on -dtlines:0 -rtp:-1 $lpquery
+
+} else {
+    $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
+    "${ScriptName} requires logparser.exe in the path."
+}

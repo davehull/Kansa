6ba02ba8048e47dba26447b1d73508e445418145
z4ns4tsu <z4ns4tsu@outlook.com>
2014-08-27 08:35:48 -0700

    Adding stack-ranking, more robustness.

--- Analysis/process/Get-WebrootListingEntropyOutliers.ps1
+++ Analysis/process/Get-WebrootListingEntropyOutliers.ps1
@@ -2,13 +2,20 @@
 .SYNOPSIS
 Get-WebrootListingEntropyOutliers.ps1
 
-Compares files entropy to the average of others with the same extension
-and outputs those more than 3 * MAD from the mean.
+Compares file entropy to the average of others with the same extension
+and outputs those more than 3 * MAD from the mean. By default, it stack
+ranks the output; this can be overwritten with the -NoStacking flag.
 
 Requires logparser.exe in your path.
 .NOTES
+OUTPUT TSV
 DATADIR WebrootListing
 #>
+
+Param(
+    [Parameter(Mandatory=$False,Position=0)]
+        [switch]$NoStacking=$False
+)
 
 function Get-Median {
     Param(
@@ -50,6 +57,42 @@ function Get-MedianAbsoluteDeviation {
     return $MAD
 }
 
+function Get-EntropyOutliers {
+    Param(
+        [Parameter(Mandatory=$True,Position=0)]
+            [psobject]$lp_results
+    )
+    
+    # LogParser doesn't have built-in formulas for deviation, so we have
+    # to do this step ourselves.
+    foreach ($lp_result in $lp_results) {
+        $extension  = $lp_result.Extension
+        $avgEntropy = $lp_result.AvgEntropy
+
+        $lp_query = @"
+            SELECT
+                FullName,
+                Length,
+                Entropy
+            FROM
+                *WebRootListing.tsv
+            WHERE
+                EXTRACT_EXTENSION(FullName) = '$extension'
+"@
+        
+        $lp_files = & logparser -stats:off -i:tsv -o:csv -dtlines:0 -fixedsep:on "$lp_query" | ConvertFrom-Csv
+        $ext_MAD  = Get-MedianAbsoluteDeviation ($lp_files).Entropy
+
+        foreach ($lp_file in $lp_files) {
+            if ([Math]::Abs($lp_file.Entropy - $avgEntropy) -gt (3 * $ext_MAD)) {
+                $o = $lp_file | Select-Object "FullName","Length","Entropy"
+                Add-Member -InputObject $o -MemberType NoteProperty -Name "EntropyAvg" -Value $avgEntropy
+                $o
+            }
+        }
+    }
+}
+
 if (Get-Command logparser.exe) {
     $lp_query = @"
         SELECT
@@ -66,39 +109,37 @@ if (Get-Command logparser.exe) {
     # Get file extensions with entropy (excludes directories).
     $lp_results = & logparser -stats:off -i:tsv -o:csv -dtlines:0 -fixedsep:on "$lp_query" | ConvertFrom-Csv
 
-    # LogParser doesn't have built-in formulas for deviation, so we have
-    # to do this step ourselves.
-    foreach ($lp_result in $lp_results) {
-        $extension = $lp_result.Extension
-        $avgEntropy = $lp_result.AvgEntropy
+    # Find the outliers.
+    $outliers = Get-EntropyOutliers $lp_results
 
-        $lp_query = @"
-            SELECT
+    if(!$NoStacking) {
+        # Stack-rank the outliers.
+        $lpquery = @"
+            SELECT 
+                COUNT(FullName,
+                    Length,
+                    Entropy,
+                    EntropyAvg) as ct,
                 FullName,
                 Length,
-                CreationTimeUtc,
-                LastAccessTimeUtc,
-                LastWriteTimeUtc,
                 Entropy,
-                PSComputerName
-            FROM
-                *WebRootListing.tsv
-            WHERE
-                EXTRACT_EXTENSION(FullName) = '$extension'
+                EntropyAvg
+            FROM STDIN
+            GROUP BY
+                FullName,
+                Length,
+                Entropy,
+                EntropyAvg
+            ORDER By
+                ct ASC
 "@
-        
-        $lp_files = & logparser -stats:off -i:tsv -o:csv -dtlines:0 -fixedsep:on "$lp_query" | ConvertFrom-Csv
-        $ext_MAD = Get-MedianAbsoluteDeviation ($lp_files).Entropy
 
-        foreach ($lp_file in $lp_files) {
-            if ([Math]::Abs($lp_file.Entropy - $avgEntropy) -gt (3 * $ext_MAD)) {
-                $o = $lp_file | Select-Object "FullName","Length","CreationTimeUtc","LastAccessTimeUtc","LastWriteTimeUtc","Entropy","PSComputerName"
-                Add-Member -InputObject $o -MemberType NoteProperty -Name "EntropyAvg" -Value $avgEntropy
-                Add-Member -InputObject $o -MemberType NoteProperty -Name "EntropyMAD" -Value $ext_MAD
-                $o
-            }
-        }
+        $outliers = $outliers | ConvertTo-Csv -NoTypeInformation | 
+            & logparser -stats:off -i:csv -dtlines:0 -rtp:-1 "$lpquery"
     }
+
+    $outliers
+    
 } else {
     $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)
     "${ScriptName} requires logparser.exe in the path."

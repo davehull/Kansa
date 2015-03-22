8c659fea7bc5294eb92a1b5c022c22131a207321
Dave Hull <dpgit@trustedsignal.com>
2015-03-22 10:19:34 -0700

    foreach changes
    Replacing foreach() statements with Foreach-Object syntax to reduce
    memory footprint

--- Modules/ASEP/Get-SvcTrigs.ps1
+++ Modules/ASEP/Get-SvcTrigs.ps1
@@ -8,16 +8,18 @@ output of this script.
 OUTPUT tsv
 #>
 
-$svctrigs = $($(foreach ($svc in (& $env:windir\system32\sc.exe query)) { 
+$svctrigs = & $env:windir\system32\sc.exe query | ForEach-Object {
+    $svc = $_ 
     if ($svc -match "SERVICE_NAME:\s(.*)") {
         & $env:windir\system32\sc qtriggerinfo $($matches[1])
     }
-})|?{$_.length -gt 1 -and $_ -notmatch "\[SC\] QueryServiceConfig2 SUCCESS|has not registered for any" })
+}|?{$_.length -gt 1 -and $_ -notmatch "\[SC\] QueryServiceConfig2 SUCCESS|has not registered for any" }
 
 function Get-LogProviderHash {
     Write-Verbose "Entering $($MyInvocation.MyCommand)"
     $LogProviders = @{}
-    foreach ($provider in (& $env:windir\system32\logman.exe query providers)) {
+    & $env:windir\system32\logman.exe query providers | ForEach-Object {
+        $provider = $_
         if ($provider -match "\{") {
             $LogName, $LogGuid = ($provider -split "{") -replace "}"
             $LogName = $LogName.Trim()
@@ -35,7 +37,9 @@ function Get-LogProviderHash {
 $LogProviders = Get-LogProviderHash
 $ServiceName = $Action = $Condition = $Data = $False
 $CondPattern = '(?<Desc>[-_ A-Za-z0-9]+)\s:\s(?<Guid>[-0-9a-fA-F]+)\s(?<Trailer>[-\[A-Za-z0-9 ]+\]*)'
-foreach($line in $svctrigs) {
+
+$svctrigs | ForEach-Object {
+    $line = $_
     $line = $line.Trim()
     if ($line -match "SERVICE_NAME:\s(?<SvcName>[-_A-Za-z0-9]+)") {
         if ($ServiceName) {

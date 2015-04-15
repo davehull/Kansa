3a80d6dca8131224b11b552dfd5b6c625ca23518
z4ns4tsu <z4ns4tsu@outlook.com>
2015-04-14 19:18:54 -0700

    Adding module ASEP\Get-Sigcheck.ps1

---
+++ Modules/ASEP/Get-Sigcheck.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+Get-Sigcheck.ps1 returns output from the SysInternals' sicheck.exe utility
+
+.NOTES
+OUTPUT tsv
+BINDEP .\Modules\bin\sigcheck.exe
+
+!! THIS SCRIPT ASUMES SIGCHECK.EXE WILL BE IN $ENV:SYSTEMROOT !!
+#>
+
+if (Test-Path "$env:SystemRoot\sigcheck.exe") {
+    & $env:SystemRoot\sigcheck.exe /accepteula -a -e -c -h -q -s -r $("$env:SystemDrive\") 2> $null | 
+        ConvertFrom-Csv |
+            ForEach-Object { $_ }
+}
+else {
+    Write-Error "Sigcheck.exe not found in $env:SystemRoot."
+}

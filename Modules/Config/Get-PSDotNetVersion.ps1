1a47e9d9fae0ab006a4181f3ccc213d79c3d3cd3
Dave Hull <dpgit@trustedsignal.com>
2015-04-21 08:21:43 -0700

    Get-PSDotNetVersion.ps1
    Adding a collector to gather information about the version of PowerShell
    and .NET from hosts.

---
+++ Modules/Config/Get-PSDotNetVersion.ps1
@@ -0,0 +1,13 @@
+﻿<#
+.SYNOPSIS
+Used for collecting some machine config info for PowerShell and .Net versioning
+.NOTES
+OUTPUT TXT
+#>
+
+"PSVersionTable:"
+$PSVersionTable | Out-String
+
+"`n.Net Version Info:"
+Get-ChildItem "$($env:windir)\Microsoft.Net\Framework" -i mscorlib.dll -r | % { $_.VersionInfo.ProductVersion }
+"Finished`n"

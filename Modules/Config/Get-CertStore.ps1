f18a6328d5264eaa14d288d12bc2515082e43009
Dave Hull <dpgit@trustedsignal.com>
2015-04-28 08:16:06 -0700

    Get-CertStore.ps1
    Changing to remove dependency on Sigcheck.exe which produces output that
    doesn't lend itself easily to scalable analysis. Switching to
    PowerShell's native Cert: provider, a simple ls -r * | fl * returns
    objects that can be turned into csv/tsv/xml for easier analysis at
    scale.

--- Modules/Config/Get-CertStore.ps1
+++ Modules/Config/Get-CertStore.ps1
@@ -1,32 +1,21 @@
 ﻿<#
 .SYNOPSIS
-Get-CertStore.ps1 uses Sysinternals' sigcheck.exe to acquire data about
-hosts' certificate stores.
-
-!! Get-CertStore.ps1 requires Sysinternals' sigcheck.exe to be in the 
-ADMIN$ share of remote hosts !!
-
-Sigcheck.exe is not packaged with Kansa. You must download it from 
-Sysinternals and drop it in .\Modules\bin\, then if you run Kansa.ps1 
-with the -Pushbin flag, it will attempt to copy sigcheck.exe to the 
-ADMIN$ share of remote hosts (targets).
-
-Running Kansa.ps1 with the -rmbin flag will cause Kansa to attempt to
-remove binaries that were copied to remote hosts via -Pushbin.
-Alternatively, you can leave the binaries on remote hosts and future
-Kansa runs may not require the -Pushbin flag.
-
+Get-CertStore.ps1 enumerates certificate stores.
+.DESCRIPTION
+Get-CertStore.ps1 uses PowerShell's Certificate provider to access and
+enumerate information about certificates on the host.
 .NOTES
 The following lines are required by Kansa.ps1. The first is an OUTPUT
 directive that tells Kansa how to tread the output from this script.
-The second tells Kansa where to find the sigcheck binary so it can copy
-it to targets when run with the -Pushbin flag.
-OUTPUT txt
-BINDEP .\Modules\bin\sigcheck.exe
+OUTPUT csv
 #>
 
-if (Test-Path "$env:SystemRoot\sigcheck.exe") {
-    & $env:SystemRoot\sigcheck.exe /accepteula -q -t '*'
-} else {
-    "sigcheck.exe not found on $env:COMPUTERNAME"
-}    
+Try {
+    Push-Location
+    Set-Location Cert:
+    ls -r * | fl *
+} Catch {
+    ("Caught exception: {0}." -f $_)
+} Finally {
+    Pop-Location
+}

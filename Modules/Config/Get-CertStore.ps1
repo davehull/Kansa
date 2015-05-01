45349d221f36fb6b7548a15fa8d88c3abae9164d
Dave Hull <dpgit@trustedsignal.com>
2015-04-30 21:32:41 -0700

    Get-CertStore.ps1
    Removing OUTPUT directive.

--- Modules/Config/Get-CertStore.ps1
+++ Modules/Config/Get-CertStore.ps1
@@ -4,10 +4,6 @@ Get-CertStore.ps1 enumerates certificate stores.
 .DESCRIPTION
 Get-CertStore.ps1 uses PowerShell's Certificate provider to access and
 enumerate information about certificates on the host.
-.NOTES
-The following lines are required by Kansa.ps1. The first is an OUTPUT
-directive that tells Kansa how to tread the output from this script.
-OUTPUT csv
 #>
 
 Try {

1a6e6071be777ccc8d7d7ed9994c1a03d2eaecd3
Dave Hull <dpgit@trustedsignal.com>
2015-06-23 14:23:26 -0700

    Kansa.ps1
    Fixing an issue that was breaking in PSv2.

--- kansa.ps1
+++ kansa.ps1
@@ -357,7 +357,7 @@ Param(
     # Need to maintain the order for "order of volatility"
     $ModuleHash = New-Object System.Collections.Specialized.OrderedDictionary
 
-    if (!(ls $ModuleScript).PSIsContainer -eq $True) {
+    if (!(ls $ModuleScript | Select-Object -ExpandProperty PSIsContainer)) {
         # User may have provided full path to a .ps1 module, which is how you run a single module explicitly
         $ModuleHash.Add((ls $ModuleScript), $ModuleArgs)
 

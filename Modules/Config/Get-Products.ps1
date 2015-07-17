f698b723517d358c199304eef7e280ffd83a6b59
exp0se <takedownz@gmail.com>
2015-07-17 12:38:49 +0300

    Get-Products replaced with faster and better version

--- Modules/Config/Get-Products.ps1
+++ Modules/Config/Get-Products.ps1
@@ -1,10 +1,10 @@
 ﻿<#
 .SYNOPSIS
 Get-Products.ps1 returns data about products that were installed via 
-the Windows installer.
+the installer.
 .NOTES
 The next line is needed by Kansa.ps1 to determine how the output from
 this script should be handled.
-OUTPUT tsv
+OUTPUT csv
 #>
-Get-WmiObject Win32_Product
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate, InstallSource

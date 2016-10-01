cdfae14b84c4a37fdbaf38aba4d6b8d022f9bd16
theflakes <theflakes@gmail.com>
2016-10-01 17:38:36 -0400

    Create Get-SchedTasks.ps1

---
+++ Modules/ASEP/Get-SchedTasks.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Get-ShedTasks.ps1 returns information about all Windows Scheduled Tasks.
+.NOTES
+The following line is required by Kansa.ps1, which uses it to determine
+how to handle the output from this script.
+OUTPUT tsv
+#>
+Get-ScheduledTask

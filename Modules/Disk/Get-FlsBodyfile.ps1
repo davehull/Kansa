018f505d12fd7d216edfb6c2a0cf50e8b68c3f77
Daniel <ddubose@users.noreply.github.com>
2017-07-28 09:05:41 -0500

    Change Get-FlsBodyfile output to CSV/Base64Gzip
    Modified the output of Get-FlsBodyfile to be a Base64GzippedStream inside of a powershell object.

--- Modules/Disk/Get-FlsBodyfile.ps1
+++ Modules/Disk/Get-FlsBodyfile.ps1
@@ -14,6 +14,11 @@ long time for large drives.
 
 !! This takes time for an entire drive !!
 
+The output of this script needs to be processed by the 
+Deserialize-KansaField.ps1 script.
+Example:
+Deserialize-KansaField.ps1
+
 .NOTES
 Next two lines are required by kansa for proper handling of output and 
 when used with -Pushbin, the BINDEP directive below tells Kansa where
@@ -35,6 +40,29 @@ Function Expand-Zip ($zipfile, $destination) {
     foreach($item in $zip.items()) {
         $shell.Namespace($destination).copyhere($item, $copyOption)
     }
+}
+
+function GetBase64GzippedStream {
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [System.IO.FileInfo]$File
+)
+    # Read profile into memory stream
+    $memFile = New-Object System.IO.MemoryStream (,[System.IO.File]::ReadAllBytes($File))
+        
+    # Create an empty memory stream to store our GZipped bytes in
+    $memStrm = New-Object System.IO.MemoryStream
+
+    # Create a GZipStream with $memStrm as its underlying storage
+    $gzStrm  = New-Object System.IO.Compression.GZipStream $memStrm, ([System.IO.Compression.CompressionMode]::Compress)
+
+    # Pass $memFile's bytes through the GZipstream into the $memStrm
+    $gzStrm.Write($memFile.ToArray(), 0, $File.Length)
+    $gzStrm.Close()
+    $gzStrm.Dispose()
+
+    # Return Base64 Encoded GZipped stream
+    [System.Convert]::ToBase64String($memStrm.ToArray())   
 } 
 
 $flspath = ($env:SystemRoot + "\fls.zip")
@@ -44,8 +72,22 @@ if (Test-Path ($flspath)) {
     $flsdest = ($env:Temp + "\fls\")
     Expand-Zip $flspath $flsdest
     if (Test-Path($flsdest + "\fls.exe")) {
-        # $sd = $env:SystemDrive
-        & $flsdest\fls.exe -r -m ($Drive) \\.\$Drive
+        
+        $suppress = & $flsdest\fls.exe -r -m ($Drive) \\.\$Drive | Out-File "$flsdest\bodyfile.txt"
+
+        $obj = "" | Select-Object FullName,Length,CreationTimeUtc,LastAccessTimeUtc,LastWriteTimeUtc,Content
+        if (Test-Path("$flsdest\bodyfile.txt")) {
+            $Target = ls "$flsdest\bodyfile.txt"
+            $obj.FullName          = $Target.FullName
+            $obj.Length            = $Target.Length
+            $obj.CreationTimeUtc   = $Target.CreationTimeUtc
+            $obj.LastAccessTimeUtc = $Target.LastAccessTimeUtc
+            $obj.LastWriteTimeUtc  = $Target.LastWriteTimeUtc
+            $obj.Content           = GetBase64GzippedStream($Target)
+        }  
+        
+        $obj
+
         $suppress = Remove-Item $flsdest -Force -Recurse
     } else {
         "Fls.zip found, but not unzipped."

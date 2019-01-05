d6bfbee41cf28f6c1dd8e1a1233487d798957e18
Jesse Moore <jesse.f.moore@gmail.com>
2019-01-05 12:11:04 -0800

    Reg Query HKCU for apps
    reg query HKCU for Microsoft SysInternals Applications installed.

---
+++ Modules/Config/Get-SysInternApps.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+Get-Process.ps1 returns data about Process.
+.NOTES
+The next line is needed by Kansa.ps1 to determine how to handle output
+from this script.
+OUTPUT TSV
+
+Contributed by Jesse Moore
+#>
+<#net localgroup administrators | select-object -skip 6 | where {
+    $_ -and $_ -notmatch "The command completed successfully"
+} |  ForEach-Object {
+    $o = "" | Select-Object Account
+    $o.Account = $_
+    $o 
+} 
+#>
+#Get Sysinternals Apps from Registry
+reg query HKCU\SOFTWARE\Sysinternals 2> null | ? {
+    $_
+} | % {
+    $o ="" | select-object SysinternApps
+    $o.SysInternApps = $_
+    $o
+}

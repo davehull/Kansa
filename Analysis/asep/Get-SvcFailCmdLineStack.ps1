1e06004beb7574b610c83e48f5150b71d30c0516
Tiago Sousa <62519990+tsousahs@users.noreply.github.com>
2020-04-06 22:02:14 +0100

    Fix logparser deprecated switch

--- Analysis/asep/Get-SvcFailCmdLineStack.ps1
+++ Analysis/asep/Get-SvcFailCmdLineStack.ps1
@@ -18,14 +18,14 @@ if (Get-Command logparser.exe) {
         COUNT(To_Lowercase(CmdLine)) as ct, 
         To_Lowercase(CmdLine) as CmdLineLC
     FROM
-        *SvcFail.tsv
+        *SvcFail.csv
     GROUP BY
         CmdLineLC
     ORDER BY
         ct ASC
 "@
 
-    & logparser -stats:off -i:csv -fixedsep:on -dtlines:0 -rtp:-1 $lpquery
+    & logparser -stats:off -i:csv -dtlines:0 -rtp:-1 $lpquery
 
 } else {
     $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)

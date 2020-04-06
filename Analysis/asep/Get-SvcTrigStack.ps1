6a77b15792bea3d3ead4b32afaf5329eecd1ef6f
Tiago Sousa <62519990+tsousahs@users.noreply.github.com>
2020-04-06 22:03:06 +0100

    Fix logparser deprecated switch

--- Analysis/asep/Get-SvcTrigStack.ps1
+++ Analysis/asep/Get-SvcTrigStack.ps1
@@ -21,7 +21,7 @@ if (Get-Command logparser.exe) {
         Subtype, 
         Data 
     FROM
-        *svctrigs.tsv 
+        *svctrigs.csv 
     GROUP BY
         ServiceName, 
         Action, 
@@ -32,7 +32,7 @@ if (Get-Command logparser.exe) {
         ct ASC
 "@
 
-    & logparser  -stats:off -i:csv -fixedsep:on -dtlines:0 -rtp:-1 $lpquery
+    & logparser  -stats:off -i:csv -dtlines:0 -rtp:-1 $lpquery
 
 } else {
     $ScriptName = [System.IO.Path]::GetFileName($MyInvocation.ScriptName)

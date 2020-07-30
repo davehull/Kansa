9329f41c84669b0e0779c9d76cd217a0a92c2f41
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2020-07-30 11:58:08 -0400

    Update Get-Netstatobject
    _ were missing through transcription

--- Modules/Net/Get-NetstatObject.ps1
+++ Modules/Net/Get-NetstatObject.ps1
@@ -17,30 +17,30 @@ Get-CimInstance -Namespace root/cimv2 -ClassName win32_process|
    Select-Object ProcessName,Path,CommandLine,ExecutablePath,Handle,ProcessId,ParentProcessID,ThreadCount,
       @{Name="User";Expression={($|Invoke-CimMethod -MethodName GetOwner).User};},
       @{Name="Domain";Expression={($|Invoke-CimMethod -MethodName GetOwner).Domain};}|
-         %{$ProcessObject[[int]$.Handle]=$}
+         %{$ProcessObject[[int]$_.Handle]=$_}
 
 Get-CimInstance -Namespace root/StandardCimv2 -ClassName MSFT_NetTCPConnection|
    Select-Object CreationTime,LocalAddress,LocalPort,OwningProcess,RemoteAddress,RemotePort,PSComputerName,
       @{Name="Protocol";Expression={"TCP"};}|
-         %{$NetworkObject[[int]$.OwningProcess]=$}
+         %{$NetworkObject[[int]$_.OwningProcess]=$_}
 
 Get-CimInstance -Namespace root/StandardCimv2 -ClassName MSFT_NetUDPEndpoint|
    Select-Object CreationTime,LocalAddress,LocalPort,OwningProcess,RemoteAddress,RemotePort,PSComputerName,
       @{Name="Protocol";Expression={"UDP"};}|
-         %{$NetworkObject[[int]$.OwningProcess]=$}
+         %{$NetworkObject[[int]$_.OwningProcess]=$_}
 
 $NetworkObject.keys|%{
-   $NetworkObject[$]|
+   $NetworkObject[$_]|
       Select-Object CreationTime,Protocol,LocalAddress,LocalPort,OwningProcess,RemoteAddress,RemotePort,PSComputerName,
-         @{Name="ProcessName";Expression={$ProcessObject[[int]$.OwningProcess].ProcessName}},
-         @{Name="Path";Expression={$ProcessObject[[int]$.OwningProcess].Path}},
-         @{Name="CommandLine";Expression={$ProcessObject[[int]$.OwningProcess].CommandLine}},
-         @{Name="ExecutablePath";Expression={$ProcessObject[[int]$.OwningProcess].ExecutablePath}},
-         @{Name="Handle";Expression={$ProcessObject[[int]$.OwningProcess].Handle}},
-         @{Name="ProcessId";Expression={$ProcessObject[[int]$.OwningProcess].ProcessId}},
-         @{Name="ParentProcessID";Expression={$ProcessObject[[int]$.OwningProcess].ParentProcessID}},
-         @{Name="ThreadCount";Expression={$ProcessObject[[int]$.OwningProcess].ThreadCount}},
-         @{Name="Domain";Expression={$ProcessObject[[int]$.OwningProcess].Domain}},
+         @{Name="ProcessName";Expression={$ProcessObject[[int]$_.OwningProcess].ProcessName}},
+         @{Name="Path";Expression={$ProcessObject[[int]$_.OwningProcess].Path}},
+         @{Name="CommandLine";Expression={$ProcessObject[[int]$_.OwningProcess].CommandLine}},
+         @{Name="ExecutablePath";Expression={$ProcessObject[[int]$_.OwningProcess].ExecutablePath}},
+         @{Name="Handle";Expression={$ProcessObject[[int]$_.OwningProcess].Handle}},
+         @{Name="ProcessId";Expression={$ProcessObject[[int]$_.OwningProcess].ProcessId}},
+         @{Name="ParentProcessID";Expression={$ProcessObject[[int]$_.OwningProcess].ParentProcessID}},
+         @{Name="ThreadCount";Expression={$ProcessObject[[int]$_.OwningProcess].ThreadCount}},
+         @{Name="Domain";Expression={$ProcessObject[[int]$_.OwningProcess].Domain}},
          @{Name="User";Expression={$ProcessObject[[int]$_.OwningProcess].User}}
    }
 }

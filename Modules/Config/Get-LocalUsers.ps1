2df08b400406a3195a15623a1c36505bebd711ab
ahans003 <anthony.hansen003@gmail.com>
2022-11-18 19:52:31 +1100

    Update Get-LocalUsers.ps1
    Added Disabled property to output

--- Modules/Config/Get-LocalUsers.ps1
+++ Modules/Config/Get-LocalUsers.ps1
@@ -1,11 +1,17 @@
 <#
 .SYNOPSIS
-Get-LocalUsers.ps1 returns a list of local user accounts detailing name, full name, caption, domain and SID.  
+Get-LocalUsers.ps1 returns the following details of each local user acccount:
+    - Disabled (bool), 
+    - Name, 
+    - Full Name, 
+    - Caption, 
+    - Domain
+    - SID.  
 
 by the dude with the moe, 18 Nov 2022. 
 .NOTES
 Next line is required by Kansa.ps1. It instructs Kansa how to handle
 the output from this script.
-OUTPUT csv
+OUTPUT tsv
 #>
-Get-WmiObject win32_UserAccount | Select-Object -Property Name, FullName, Caption, Domain, SID
+Get-WmiObject win32_UserAccount | Select-Object -Property Disabled, Name, FullName, Caption, Domain, SID
